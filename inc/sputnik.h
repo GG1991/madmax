211419e5798fdff65160bcb853514563dd386004
GG1991 <giuntoli1991@gmail.com>
2017-06-18 14:56:49 +0200

    scheme ID definition on sputnik main header

--- inc/sputnik.h
+++ inc/sputnik.h
@@ -14,9 +14,13 @@
 #define MICRO              2     // MICRO IDs and colors
 #define NBUF               256   // Buffers length for read from a file
 
-#define APPROACH_MACRO     1
-#define APPROACH_MICRO     2
-#define APPROACH_MACMIC_1  3
+
+// Execution schemes
+#define SCHEME_MACRO_MICRO_COU     1   // Coupled execution
+#define SCHEME_MACRO_PART_MESH     2   // Standalone execution
+#define SCHEME_MICRO_PART_MESH     3
+#define SCHEME_MACRO_PDE_ALONE     4
+#define SCHEME_MICRO_PDE_ALONE     5
 
 #define PARMETIS_GEOMKWAY  1
 #define PARMETIS_GEOM      2
@@ -32,7 +36,7 @@ int *id_vec;    // ID vector size = #proc (info of which ID has each rank
 int nproc_mac;  // number of macro processes 
 int nproc_mic;  // number of micro processes
 int nstru_mic;  // number of micro structures
-int comm_apro;  // communication approach
+int scheme;     // communication approach
 
 
 /*****************************************************************************************************
