53b2fa096afcd2d7d06bcda2d2150675cfc07866
GG1991 <giuntoli1991@gmail.com>
2017-06-20 17:26:34 +0200

    debug_coupling.sh is able to run normal

--- exs/ex1/debug_coupling.sh
+++ exs/ex1/debug_coupling.sh
@@ -4,8 +4,8 @@
 #break_mic=( 'mic_main.c:32' ) 
 #break_mac=( 'mac_comm.c:100' ) 
 #break_mic=( 'mic_comm.c:104' ) 
-break_mac=( 'mac_comm.c:107' ) 
-break_mic=( 'mic_comm.c:111' ) 
+break_mac=( 'mac_comm.c:101' ) 
+break_mic=( 'mic_comm.c:105' ) 
 
 
 # BREAKPOINTS
@@ -23,5 +23,13 @@ exopt_mic+="-ex 'r'"
 
 gdbcomm_mac="gdb $exopt_mac --args  ../../macro/macro ../../macro/exs/ex1/ex1.mac"
 gdbcomm_mic="gdb $exopt_mic --args  ../../micro/micro ../../macro/exs/ex1/ex1.mac"
+exec_mac="../../macro/macro ../../macro/exs/ex1/ex1.mac"
+exec_mic="../../micro/micro ../../macro/exs/ex1/ex1.mac"
 
-mpirun -np 1 xterm -e "$gdbcomm_mac" : -np 1 xterm -e "$gdbcomm_mic"
+#mpirun -np 1 xterm -e "$gdbcomm_mac" : -np 2 xterm -e "$gdbcomm_mic"
+if [ "$#" -gt 0 ];then
+   echo "mpirun -np 1 "$exec_mac" : -np 2 "$exec_mic""
+   eval mpirun -np 1 "$exec_mac" : -np 2 "$exec_mic"
+else
+   mpirun -np 2 xterm -e "$gdbcomm_mac" : -np 2 xterm -e "$gdbcomm_mic"
+fi
