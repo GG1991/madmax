e68e7ab830bb3232e5b3949e6813dab1a0e989a1
Guido Giuntoli <giuntoli1991@gmail.com>
2017-07-20 00:43:00 +0200

    changing script name to prepeare cube mesh

--- exs/ex1/set_problem.sh
+++ exs/ex1/set_problem.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-if [ $# -ne 2 ];then
-  echo "2 arguments should be given <N> problem size <NP> number of processes"
+if [ $# -ne 1 ];then
+  echo "One argument should be given <N> problem size" 
   exit 1
 fi
 N=$1
-NP=$2
 
 cd ../../meshes/cube_unif
 m4 -DN_M4=$N cube.geo.m4 > cube.geo
@@ -13,4 +12,3 @@ gmsh -3 cube.geo 2&> null
 cd ../../exs/ex1
 printf "0K\n"
 
-./coupling.sh -log_trace
