190f53457de494de31071f2179e1e7cb973d0a8f
GG1991 <giuntoli1991@gmail.com>
2017-07-27 14:22:31 +0200

    adding more executing options to run.sh

--- exs/ex1/run.sh
+++ exs/ex1/run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-./mpirun -np 1 ../../macro/macro ex1.spu \
-         -pc_type lu \
+#./mpirun -np 1 ../../macro/macro ex1.spu \
+#         -ksp_type cg  \
+#	 -p_vtk   2  \
+#       : -np 1 ../../micro/micro ex1.spu
+
+#./mpirun -np 1 ../../macro/macro ex1.spu \
+#         -ksp_type cg  \
+#         -pc_type lu  \
+#	 -p_vtk   2  \
+#       : -np 1 ../../micro/micro ex1.spu
+
+./mpirun -np 2 ../../macro/macro ex1.spu \
+         -ksp_type cg  \
 	 -p_vtk   2  \
        : -np 1 ../../micro/micro ex1.spu
