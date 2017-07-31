7d0ea2429a9b09caca6757072378b359755e5811
GG1991 <giuntoli1991@gmail.com>
2017-07-31 12:00:13 +0200

    enabling -option_left

--- exs/ex1/run_macro.sh
+++ exs/ex1/run_macro.sh
@@ -4,6 +4,6 @@ NM=2
 
 ./mpirun -np $NM ../../macro/macro ex1.spu \
     -ksp_type cg                    \
-    -ksp_rtol 1.0e-13
-#    -options_left 0
+    -ksp_rtol 1.0e-13               \
+    -options_left 0
 #    -coupl 1                        \
