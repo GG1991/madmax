2f878033b93f678004cd465647188fddc6e06de2
GG1991 <giuntoli1991@gmail.com>
2017-08-04 14:45:52 +0200

    using bash functions on run and debug scripts

--- exs/ex1/debug_micro.sh
+++ exs/ex1/debug_micro.sh
@@ -2,11 +2,11 @@
 
 NM=1
 
-#break_mic=( 'mic_main.c:xx' ) 
+#break_mic=( 'mic_main.c:354' ) 
 #break_mic=( 'spu_mesh.c:xx' ) 
 #break_mic=( 'micmic.c:xx' ) 
 #break_mic=( 'mic_alloc.c:xx' ) 
-#break_mic=( 'spu_assembly.c:xx' ) 
+#break_mic=( 'spu_assembly.c:52' ) 
 #break_mic=( 'mic_boundary.c:xx' ) 
 
 # BREAKPOINTS
@@ -18,6 +18,8 @@ exopt_mic+="-ex 'r'"
 
 gdbcomm_mic="gdb $exopt_mic --args  ../../micro/micro "
 gdbcomm_mic+=" -mesh ../../meshes/cube_unif/cube.msh"
+
+function ex_common {
 eval ./mpirun -np $NM xterm -e gdb "$exopt_mic" --args  ../../micro/micro   \
        -input ex1.spu                         \
        -mesh ../../meshes/cube_unif/cube.msh  \
@@ -25,12 +27,17 @@ eval ./mpirun -np $NM xterm -e gdb "$exopt_mic" --args  ../../micro/micro   \
        -ksp_rtol 1.0e-13                      \
        -options_left 0                        \
        -print_part
+}
 
-#eval ./mpirun -np $NM valgrind --log-file=\"valgrind.out\" --leak-check=full ../../micro/micro \
-#       -input ex1.spu                         \
-#       -mesh ../../meshes/cube_unif/cube.msh  \
-#       -ksp_type cg                           \
-#       -ksp_rtol 1.0e-13                      \
-#       -options_left 0                        \
-#       -print_part
-#
+function ex_valgrind {
+eval ./mpirun -np $NM valgrind --log-file=\"valgrind.out\" --leak-check=full ../../micro/micro \
+       -input ex1.spu                         \
+       -mesh ../../meshes/cube_unif/cube.msh  \
+       -ksp_type cg                           \
+       -ksp_rtol 1.0e-13                      \
+       -options_left 0                        \
+       -print_part
+}
+
+#ex_common
+#ex_valgrind
--- exs/ex1/run_micro.sh
+++ exs/ex1/run_micro.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+function nproc_1 {
 NM=1
 
 ./mpirun -np $NM ../../micro/micro         \
@@ -11,14 +13,22 @@ NM=1
     -options_left 0                        \
     -print_disp                            \
     -print_part
+}
 
-#NM=2
-#
-#./mpirun -np $NM ../../micro/micro         \
-#    -input ex1.spu                         \
-#    -mesh ../../meshes/cube_unif/cube.msh  \
-#    -ksp_type cg                           \
-#    -ksp_rtol 1.0e-13                      \
-#    -options_left 0                        \
-#    -print_disp                            \
-#    -print_part
+
+function nproc_2 {
+NM=2
+
+./mpirun -np $NM ../../micro/micro         \
+    -input ex1.spu                         \
+    -mesh ../../meshes/cube_unif/cube.msh  \
+    -ksp_type cg                           \
+    -ksp_rtol 1.0e-13                      \
+    -options_left 0                        \
+    -print_disp                            \
+    -log_trace macro_trace                 \
+    -print_part
+}
+
+#nproc_1
+#nproc_2
