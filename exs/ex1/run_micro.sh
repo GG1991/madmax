1f666682b9318f1e6b2103c43bb13c6d66a5a21b
GG1991 <giuntoli1991@gmail.com>
2017-08-04 10:40:09 +0200

    adding in script run_micro the posibbility of running with multiple processes

--- exs/ex1/run_micro.sh
+++ exs/ex1/run_micro.sh
@@ -11,3 +11,14 @@ NM=1
     -options_left 0                        \
     -print_disp                            \
     -print_part
+
+#NM=2
+#
+#./mpirun -np $NM ../../micro/micro         \
+#    -input ex1.spu                         \
+#    -mesh ../../meshes/cube_unif/cube.msh  \
+#    -ksp_type cg                           \
+#    -ksp_rtol 1.0e-13                      \
+#    -options_left 0                        \
+#    -print_disp                            \
+#    -print_part
