3690d0e29d6a96eb69b0cdb6ea6bd3f2c93fcc9d
GG1991 <giuntoli1991@gmail.com>
2017-08-24 19:57:20 +0200

    adding macro calculation script

--- exs/ex1/run_macro.sh
+++ exs/ex1/run_macro.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
 
-function nproc_4 {
+function cube {
 
 NM=4
 ./mpirun -np $NM ../../macro/macro         \
     -input ex1.spu                         \
     -mesh ../../meshes/cube_unif/cube.msh  \
-    -ksp_type cg                           \
-    -ksp_rtol 1.0e-13                      \
-    -options_left 0                        \
-    -log_trace macro_trace                 \
-    -print_part
+    -ksp_type cg \
+    -ksp_rtol 1.0e-13 \
+    -log_trace macro_trace \
+    -print_pvtui \
+    -options_left 0
 }
-nproc_4
+
+function struct_fiber {
+
+NM=4
+./mpirun -np $NM ../../macro/macro \
+    -input ex1.spu \
+    -mesh ../../meshes/cube_fiber/struct_fiber.msh \
+    -mesh_gmsh \
+    -ksp_type cg \
+    -ksp_rtol 1.0e-13 \
+    -log_trace macro_trace                 \
+    -print_vtu \
+    -tf 1.0 \
+    -dt 1.0 \
+    -options_left 0
+}
+#struct_fiber
