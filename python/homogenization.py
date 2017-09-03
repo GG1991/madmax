4521b44c8119c6362b4ab5db67c52a0a2a44faf4
Guido Giuntoli <giuntoli1991@gmail.com>
2017-09-04 00:27:11 +0200

    fixing some bug on homogenization and testing with only one elemnt

--- python/homogenization.py
+++ python/homogenization.py
@@ -37,6 +37,7 @@ def elem_residue(e, elem, x, be):
     index[[n*2+0, n*2+1]] = [elem[e,n]*2+0,elem[e,n]*2+1]
   elem_disp = x[index]
   stress_gp = np.zeros( 3 )
+  print "elem_disp",elem_disp
 
   for gp in range(0, xp.shape[0]):
 
@@ -47,6 +48,7 @@ def elem_residue(e, elem, x, be):
       B[2,[sh*2+0, sh*2+1]] = [dsh[sh,1,gp], dsh[sh,0,gp]]
 
     stress_gp = np.dot(B,elem_disp)
+    print "stress_gp",stress_gp
     be += np.dot(B.transpose(), stress_gp)
 
   return;
@@ -55,8 +57,8 @@ def elem_residue(e, elem, x, be):
 #
 # main program
 #
-nx = 3
-ny = 3
+nx = 2
+ny = 2
 lx = 1.0
 ly = 1.0
 
@@ -90,7 +92,6 @@ C = np.multiply(C,E*(1-nu)/((1+nu)*(1-2*nu)))
 J  = np.zeros( (nx*ny*2 + n_bc*2,nx*ny*2 + n_bc*2) )
 x  = np.zeros( nx*ny*2 + n_bc*2 )
 dx = np.zeros( nx*ny*2 + n_bc*2)
-b  = np.zeros( nx*ny*2 + n_bc*2 )
 ke = np.zeros( (4*2,4*2) )
 be = np.zeros( 4*2 )
 D  = np.zeros( (3,n_bc*2) )
@@ -120,14 +121,14 @@ ux_y0_ind = np.arange(2          ,nx*2-2,2)
 uy_y0_ind = np.arange(3          ,nx*2-2,2)
 ux_y1_ind = np.arange(((ny-1)*nx+1)*2,ny*nx*2-2,2)
 uy_y1_ind = np.arange(((ny-1)*nx+1)*2+1,ny*nx*2-2,2)
-#print ux_x0_ind, "\n"
-#print ux_x1_ind, "\n"
-#print uy_x0_ind, "\n"
-#print uy_x1_ind, "\n"
-#print ux_y0_ind, "\n"
-#print uy_y0_ind, "\n"
-#print ux_y1_ind, "\n"
-#print uy_y1_ind, "\n"
+#print "ux_x0_ind",ux_x0_ind, "\n"
+#print "ux_x1_ind",ux_x1_ind, "\n"
+#print "uy_x0_ind",uy_x0_ind, "\n"
+#print "uy_x1_ind",uy_x1_ind, "\n"
+#print "ux_y0_ind",ux_y0_ind, "\n"
+#print "uy_y0_ind",uy_y0_ind, "\n"
+#print "ux_y1_ind",ux_y1_ind, "\n"
+#print "uy_y1_ind",uy_y1_ind, "\n"
 
 n_ind_bc = np.concatenate((ux_y0_ind, ux_x1_ind, ux_y1_ind, ux_x0_ind))/2
 x_bc = np.concatenate((ux_y0_ind,uy_y0_ind,ux_x1_ind,uy_x1_ind,ux_y1_ind,uy_y1_ind,ux_x0_ind,uy_x0_ind))
@@ -137,7 +138,9 @@ print "n_ind_bc",n_ind_bc
 for i in range(0, n_bc):
   D[0,i*2+0] = coor[n_ind_bc[i],0]   ; D[0,i*2+1] = 0
   D[1,i*2+0] = 0                     ; D[1,i*2+1] = coor[n_ind_bc[i],1]
-  D[2,i*2+0] = coor[n_ind_bc[i],1]/2 ; D[2,i*2+1] = coor[n_ind_bc[i]/2,0]
+  D[2,i*2+0] = coor[n_ind_bc[i],1]/2 ; D[2,i*2+1] = coor[n_ind_bc[i],0]/2
+
+print "D^T\n",np.transpose(D)
 
 #calculate elemental matrix
 elem_matrix( ke )
@@ -150,12 +153,13 @@ for e in range(0, elem.shape[0]):
   J[np.ix_(index,index)] += ke
 
 # set BCs on J
-J[x_bc[:], nx*ny*2 + np.arange(x_bc.size)] = 1e7;
-J[nx*ny*2 + np.arange(x_bc.size), x_bc[:]] = 1e7;
+J[x_bc[:], nx*ny*2 + np.arange(x_bc.size)] = 1e6;
+J[nx*ny*2 + np.arange(x_bc.size), x_bc[:]] = 1e6;
 
 e_mac = np.array([[1.0],[0.0],[0.0]])
 
 # assembly b 
+b  = np.zeros( nx*ny*2 + n_bc*2 )
 for e in range(0, elem.shape[0]):
   elem_residue(e, elem, x, be)
   for n in range( 0, elem.shape[1]):
@@ -163,12 +167,29 @@ for e in range(0, elem.shape[0]):
   b[index] += be
 
 ub = x[x_bc] 
+delta = x[nx*ny*2:]
 b[nx*ny*2 + np.arange(x_bc.size)] = ub - np.transpose(np.dot(D.transpose(),e_mac))
+b = -b
+
+print "delta", delta
 print x_bc
 print nx*ny*2 + x_bc
 print b
 x = np.linalg.solve(J, b)
-print x
+
+b  = np.zeros( nx*ny*2 + n_bc*2 )
+for e in range(0, elem.shape[0]):
+  elem_residue(e, elem, x, be)
+  for n in range( 0, elem.shape[1]):
+    index[[n*2+0, n*2+1]] = [elem[e,n]*2+0,elem[e,n]*2+1]
+  b[index] += be
+
+ub = x[x_bc] 
+delta = x[nx*ny*2:]
+print "D^T*e",np.transpose(np.dot(D.transpose(),e_mac))
+b[nx*ny*2 + np.arange(x_bc.size)] = ub - np.transpose(np.dot(D.transpose(),e_mac))
+b = -b
+print b
 
 # plot the matrix
 plt.matshow(J)
