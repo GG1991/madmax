195649a5d9cd18abc36b9379b55f5be0d628f047
GG1991 <giuntoli1991@gmail.com>
2017-09-03 11:14:52 +0200

    elemental matrix calculation

--- python/homogenization.py
+++ python/homogenization.py
@@ -8,14 +8,57 @@
 
 import numpy as np
 import math as math
-from scipy.sparse import csr_matrix
+
+############################################################
+def elem_matrix(ke):
+
+  xp = np.zeros( (4,2) )
+  xp[0,:] = [(-1/math.sqrt(3.0)+1)/2 , (-1/math.sqrt(3.0)+1)/2] 
+  xp[1,:] = [(+1/math.sqrt(3.0)+1)/2 , (-1/math.sqrt(3.0)+1)/2] 
+  xp[2,:] = [(+1/math.sqrt(3.0)+1)/2 , (+1/math.sqrt(3.0)+1)/2] 
+  xp[3,:] = [(-1/math.sqrt(3.0)+1)/2 , (+1/math.sqrt(3.0)+1)/2] 
+
+  wp = np.ones( (4) )
+  wp = np.multiply(wp,0.25)
+
+  dsh = np.zeros( (4,2,4) ) # num_sh, x_dir, num_gp
+  for gp in range(0, xp.shape[0]):
+    dsh[0,0,gp] = -1.0*(1-xp[gp,1]); dsh[0,1,gp] = (1-xp[gp,0])*-1.0
+    dsh[1,0,gp] = +1.0*(1-xp[gp,1]); dsh[1,1,gp] = (0+xp[gp,0])*-1.0
+    dsh[2,0,gp] = +1.0*(0+xp[gp,1]); dsh[2,1,gp] = (0+xp[gp,0])*+1.0
+    dsh[3,0,gp] = -1.0*(0+xp[gp,1]); dsh[3,1,gp] = (1-xp[gp,0])*+1.0
+
+  # define constitutive tensor
+  nu = 0.3; E  = 1e6
+  C = np.array([
+  [1         ,nu/(1-nu) ,0                  ],
+  [nu/(1-nu) ,1         ,0                  ],
+  [0         ,0         ,(1-2*nu)/(2*(1-nu))]
+  ])
+  C = np.multiply(C,E*(1-nu)/((1+nu)*(1-2*nu)))
+
+  for gp in range(0, xp.shape[0]):
+
+    B = np.zeros( (3,4*2) )
+    for sh in range(0, dsh.shape[0]):
+      B[0,[sh*2+0, sh*2+1]] = [dsh[sh,0,gp], 0           ]
+      B[1,[sh*2+0, sh*2+1]] = [0           , dsh[sh,0,gp]]
+      B[2,[sh*2+0, sh*2+1]] = [dsh[sh,1,gp], dsh[sh,0,gp]]
+
+    ke += reduce(np.dot,[B.transpose(),C,B])
+
+  print ke, "\n"
+  return;
+
+############################################################
 
 nx = 3
 ny = 3
 lx = 1.0
 ly = 1.0
 
-A = np.zeros( (nx*ny*2,nx*ny*2) )
+A  = np.zeros( (nx*ny*2,nx*ny*2) )
+ke = np.zeros( (4*2,4*2) )
 
 elem = np.zeros( ((nx-1)*(ny-1),4) )
 coor = np.zeros( (nx*ny,2) )
@@ -33,22 +76,10 @@ for i in range(0, ny):
     coor[i*nx+j,1] = i*ly/(ny-1)
 
 
-xp = np.zeros( (4,2) )
-
-xp[0,:] = [(-1/math.sqrt(3.0)+1)/2 , (-1/math.sqrt(3.0)+1)/2] 
-xp[1,:] = [(+1/math.sqrt(3.0)+1)/2 , (-1/math.sqrt(3.0)+1)/2] 
-xp[2,:] = [(+1/math.sqrt(3.0)+1)/2 , (+1/math.sqrt(3.0)+1)/2] 
-xp[3,:] = [(-1/math.sqrt(3.0)+1)/2 , (+1/math.sqrt(3.0)+1)/2] 
-
-wp = np.zeros( (4) )
-
-wp[0] = 0.25
-wp[1] = 0.25
-wp[2] = 0.25
-wp[3] = 0.25
 
 
 print elem, "\n"
 print coor, "\n"
-print xp, "\n"
-print wp, "\n"
+
+elem_matrix( ke )
+
