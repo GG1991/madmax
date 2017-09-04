df33fa841abba9c69db2f3fa7e8278567869f403
GG1991 <giuntoli1991@gmail.com>
2017-09-04 14:08:58 +0200

    adding homogenization structure to do a cleaner workflow of micro

---
+++ micro/inc/homogenization.h
@@ -0,0 +1,32 @@
+/*
+   Homogenization Variables
+*/
+
+#define HOMO_TAYLOR           1
+#define HOMO_LINEAR           2
+#define HOMO_LINEAR_HEXA      3
+#define LD_LAGRAN             4
+
+/*
+   Linear deformation using Lagrange multipliers
+*/
+
+typedef struct _homog_ld_lagran_t{
+
+  int nnods_bc;
+  int *index;
+  double *ub_val;
+
+}homog_ld_lagran_t;
+
+typedef struct _homog_t{
+
+  int type;
+  int dim;
+  void *st;
+
+}homog_t;
+
+homog_t homo;
+
+int mic_init_homo();
