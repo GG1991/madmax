f4a4635ee41fe7604e8d79e1472d25477da63272
GG1991 <giuntoli1991@gmail.com>
2017-09-08 17:56:53 +0200

    adding a classical implementation of LD without Lagrange multipliers

--- micro/src/mic_homogenize.c
+++ micro/src/mic_homogenize.c
@@ -57,81 +57,8 @@ int micro_homogenize_taylor(MPI_Comm PROBLEM_COMM, double strain_mac[6], double 
 /****************************************************************************************************/
 int micro_homogenize_linear(MPI_Comm MICRO_COMM, double strain_mac[6], double strain_ave[6], double stress_ave[6])
 {
-  int    ierr, nr_its=-1, max_its=3;
-  double norm_tol=1.0e-8, norm=2*norm_tol;
-//  double ksp_norm=-1.0;
 
-  ierr = micro_apply_bc_linear(strain_mac, &x, &A, &b, SET_DISPLACE);CHKERRQ(ierr);
-
-  while( nr_its < max_its && norm > norm_tol )
-  {
-    ierr = assembly_residual_sd( &x, &b);CHKERRQ(ierr);
-    ierr = micro_apply_bc_linear(strain_mac, &x, &A, &b, SET_RESIDUAL);CHKERRQ(ierr);
-    ierr = VecNorm(b,NORM_2,&norm);CHKERRQ(ierr);
-    if( !(norm > norm_tol) )break;
-    ierr = VecScale(b,-1.0); CHKERRQ(ierr);
-    ierr = assembly_jacobian_sd(&A);
-    ierr = micro_apply_bc_linear(strain_mac, &x, &A, &b, SET_JACOBIAN);CHKERRQ(ierr);
-    ierr = KSPSolve(ksp,b,dx);CHKERRQ(ierr);
-    ierr = VecAXPY( x, 1.0, dx); CHKERRQ(ierr);
-    nr_its ++;
-  }
-
-  ierr = calc_ave_strain_stress(MICRO_COMM, &x, strain_ave, stress_ave);CHKERRQ(ierr);
-
-  return 0;
-}
-/****************************************************************************************************/
-int micro_homogenize_linear_hexa(MPI_Comm MICRO_COMM, double strain_mac[6], double strain_ave[6], double stress_ave[6])
-{
-
-  /*
-     Performs linear homogenization of the RVE cell acording to <strain_mac[6]>
-   */
-
-  int ierr, nr_its = -1; 
-  //  int kspits = -1;
-  double norm = -1.0, norm_tol = 1.0e-8, max_its = 3; 
-  //  double ksp_norm = -1.0;
-
-  ierr = micro_apply_bc_linear_hexa(strain_mac, &x, &A, &b, SET_DISPLACE);CHKERRQ(ierr);
-
-  while( nr_its < max_its && norm > norm_tol )
-  {
-    ierr = assembly_residual_sd( &x, &b);CHKERRQ(ierr);
-    ierr = micro_apply_bc_linear_hexa(strain_mac, &x, &A, &b, SET_RESIDUAL);CHKERRQ(ierr);
-    ierr = VecNorm(b,NORM_2,&norm);CHKERRQ(ierr);
-    if( !(norm > norm_tol) )break;
-    ierr = VecScale(b,-1.0); CHKERRQ(ierr);
-    ierr = assembly_jacobian_sd(&A);
-    ierr = micro_apply_bc_linear_hexa(strain_mac, &x, &A, &b, SET_JACOBIAN);CHKERRQ(ierr);
-    ierr = KSPSolve(ksp,b,dx);CHKERRQ(ierr);
-    ierr = VecAXPY( x, 1.0, dx); CHKERRQ(ierr);
-    nr_its ++;
-  }
-
-  ierr = calc_ave_strain_stress(MICRO_COMM, &x, strain_ave, stress_ave);CHKERRQ(ierr);
-
-  return 0;
-}
-/****************************************************************************************************/
-int mic_homogenize_ld_lagran(MPI_Comm MICRO_COMM, double strain_mac[6], double strain_ave[6], double stress_ave[6])
-{
-  /*
-     Here we apply the Lagrage multiplier procedure 
-     to stablish the linear displacement boundary conditions. 
-     The equations can be written:
-     fa(u)=0
-     fb(u)-ub=0
-     ub-D^Te=0
-
-         | A    ei |       | f       |     | x_val |
-     J = |         |  b =  |         | x = |       |
-         | ei^T 0  |       | ub-D^Te |     | l_val |
-
-     the "f" contains fa and fb mixed depending on the node numeration
-  */
-  int    ierr, nr_its=-1, max_its=3, nnods_bc, i, d, k, n_loc, *ixb;
+  int    ierr, nr_its=-1, max_its=6, nnods_bc, i, d, k, n_loc, *ixb;
   double norm_tol=1.0e-8, norm=2*norm_tol, ub, strain_matrix[3][3], *x_arr, *b_arr;
 
   if(dim==2){
@@ -156,6 +83,98 @@ int mic_homogenize_ld_lagran(MPI_Comm MICRO_COMM, double strain_mac[6], double s
       for(k=0;k<dim;k++){
 	ub += strain_matrix[d][k]*coord[n_loc * 3 + k];
       }
+      ((homog_ld_t*)homo.st)->ub_val[i*dim+d] = ub;
+    }
+  }
+  for(i=0; i<nnods_bc; i++){
+    for(d=0;d<dim;d++){
+      x_arr[ixb[i*dim+d]] = ((homog_ld_t*)homo.st)->ub_val[i*dim+d]; 
+    }
+  }
+
+  while( nr_its < max_its && norm > norm_tol )
+  {
+    ierr = assembly_residual_sd( &x, &b);CHKERRQ(ierr);
+    ierr = VecGetArray(b, &b_arr);CHKERRQ(ierr);
+    for(i=0; i<nnods_bc; i++){
+      for(d=0;d<dim;d++){
+	b_arr[ixb[i*dim+d]] = 0.0;
+      }
+    }
+    ierr = VecRestoreArray(b, &b_arr);CHKERRQ(ierr);
+    ierr = VecNorm(b,NORM_2,&norm);CHKERRQ(ierr);
+    PetscPrintf(MICRO_COMM,"|b| = %lf \n",norm);
+    if( !(norm > norm_tol) )break;
+    ierr = VecScale(b,-1.0); CHKERRQ(ierr);
+    ierr = assembly_jacobian_sd(&A);
+    ierr = MatZeroRowsColumns(A, nnods_bc*dim, ixb, 1.0, NULL, NULL); CHKERRQ(ierr);
+    ierr = KSPSolve(ksp,b,dx);CHKERRQ(ierr);
+
+    ierr = VecAXPY( x, 1.0, dx); CHKERRQ(ierr);
+
+    ierr = MatMult(A, dx, b1);CHKERRQ(ierr);
+    
+    if( flag_print & (1<<PRINT_PETSC) ){
+      ierr = PetscViewerASCIIOpen(MICRO_COMM,"A.dat",&viewer); CHKERRQ(ierr);
+      ierr = MatView(A,viewer); CHKERRQ(ierr);
+      ierr = PetscViewerASCIIOpen(MICRO_COMM,"b.dat",&viewer); CHKERRQ(ierr);
+      ierr = VecView(b,viewer); CHKERRQ(ierr);
+      ierr = PetscViewerASCIIOpen(MICRO_COMM,"b1.dat",&viewer); CHKERRQ(ierr);
+      ierr = VecView(b1,viewer); CHKERRQ(ierr);
+      ierr = PetscViewerASCIIOpen(MICRO_COMM,"x.dat",&viewer); CHKERRQ(ierr);
+      ierr = VecView(x,viewer); CHKERRQ(ierr);
+      ierr = PetscViewerASCIIOpen(MICRO_COMM,"dx.dat",&viewer); CHKERRQ(ierr);
+      ierr = VecView(x,viewer); CHKERRQ(ierr);
+    }
+
+    nr_its ++;
+  }
+
+  ierr = calc_ave_strain_stress(MICRO_COMM, &x, strain_ave, stress_ave);CHKERRQ(ierr);
+
+  return 0;
+}
+/****************************************************************************************************/
+int mic_homogenize_ld_lagran(MPI_Comm MICRO_COMM, double strain_mac[6], double strain_ave[6], double stress_ave[6])
+{
+  /*
+     Here we apply the Lagrage multiplier procedure 
+     to stablish the linear displacement boundary conditions. 
+     The equations can be written:
+     fa(u)=0
+     fb(u)-ub=0
+     ub-D^Te=0
+
+         | A    ei |       | f       |     | x_val |
+     J = |         |  b =  |         | x = |       |
+         | ei^T 0  |       | ub-D^Te |     | l_val |
+
+     the "f" contains fa and fb mixed depending on the node numeration
+  */
+  int    ierr, nr_its=-1, max_its=6, nnods_bc, i, d, k, n_loc, *ixb;
+  double norm_tol=1.0e-8, norm=2*norm_tol, ub, strain_matrix[3][3], *x_arr, *b_arr;
+
+  if(dim==2){
+    strain_matrix[0][0]=strain_mac[0]; strain_matrix[0][1]=strain_mac[2];
+    strain_matrix[1][0]=strain_mac[2]; strain_matrix[1][1]=strain_mac[1];
+  }
+  else if(dim==3){
+    strain_matrix[0][0]=strain_mac[0]; strain_matrix[0][1]=strain_mac[3]; strain_matrix[0][2]=strain_mac[5];
+    strain_matrix[1][0]=strain_mac[3]; strain_matrix[1][1]=strain_mac[1]; strain_matrix[1][2]=strain_mac[4];
+    strain_matrix[2][0]=strain_mac[5]; strain_matrix[2][1]=strain_mac[4]; strain_matrix[2][2]=strain_mac[2];
+  }
+
+  nnods_bc = ((homog_ld_lagran_t*)homo.st)->nnods_bc;
+  ixb = ((homog_ld_lagran_t*)homo.st)->index;
+
+  // first we fill the value ub_val in <homog_ld_lagran_t> structure
+  for(i=0; i<nnods_bc; i++){
+    for(d=0;d<dim;d++){
+      n_loc = ixb[i*dim+d]/dim;
+      ub = 0.0;
+      for(k=0;k<dim;k++){
+	ub += strain_matrix[d][k]*coord[n_loc * 3 + k];
+      }
       ((homog_ld_lagran_t*)homo.st)->ub_val[i*dim+d] = ub;
     }
   }
@@ -164,6 +183,7 @@ int mic_homogenize_ld_lagran(MPI_Comm MICRO_COMM, double strain_mac[6], double s
   {
 
     /* internal forces */
+    ierr = VecGetArray(x, &x_arr);CHKERRQ(ierr);
     ierr = assembly_residual_sd(&x, &b);CHKERRQ(ierr);
     /* fb - delta */
     ierr = VecGetArray(b, &b_arr);CHKERRQ(ierr);
@@ -179,6 +199,7 @@ int mic_homogenize_ld_lagran(MPI_Comm MICRO_COMM, double strain_mac[6], double s
       }
     }
     ierr = VecRestoreArray(b, &b_arr);CHKERRQ(ierr);
+    ierr = VecRestoreArray(x, &x_arr);CHKERRQ(ierr);
 
     ierr = VecNorm(b,NORM_2,&norm);CHKERRQ(ierr);
     PetscPrintf(MICRO_COMM,"|b| = %lf \n",norm);
@@ -245,6 +266,9 @@ int micro_homogenize(MPI_Comm MICRO_COMM, double strain_mac[6], double strain_av
 
      HOMO_TAYLOR, HOMO_LINEAR, HOMO_PERIOD, HOMO_LD_LAGRAN_SEQ, HOMO_EXP
 
+     LD            > Linear Displacements
+     LD_LAGRAN_SEQ > Linear Displacement with Lagrangian multiplier BC
+
      HOMO_TAYLOR > no need of calculating a displacement field
    */
   int ierr;
@@ -252,7 +276,7 @@ int micro_homogenize(MPI_Comm MICRO_COMM, double strain_mac[6], double strain_av
   if(homo.type==HOMO_TAYLOR){
     ierr = micro_homogenize_taylor(MICRO_COMM, strain_mac, strain_ave, stress_ave);CHKERRQ(ierr);
   }
-  else if(homo.type==HOMO_LINEAR){
+  else if(homo.type==LD){
     ierr = micro_homogenize_linear(MICRO_COMM, strain_mac, strain_ave, stress_ave);CHKERRQ(ierr);
   }
   else if(homo.type==HOMO_LINEAR_HEXA){
@@ -272,6 +296,7 @@ int mic_init_homo(void)
 {
   
   if(homo.type==LD_LAGRAN_SEQ){
+
     /*
        a) Count how many nodes (nnods_bc) belongs 
        to the boundary search in the boundary_list
@@ -355,6 +380,124 @@ int mic_init_homo(void)
     }
 
   }
+
+  if(homo.type==LD){
+
+    /*
+       a) Count how many nodes (nnods_bc) belongs 
+       to the boundary search in the boundary_list
+       (deleted the ones which are repeated)
+       b) Allocate <index> and <ub_val>
+
+       <index>  correspond to the global numeration where 
+       the values should be assembled on the matrix and 
+       vector
+       <ub_val> corresponds to the product <D^T . e_mac>
+    */
+
+    homo.st = malloc(sizeof(homog_ld_t));
+
+    int nnods_bc=0, nnods_bc_a, c, n_glo, n_loc, n_orig, i, d, *p, *aux_nod;
+    node_list_t *pb,*pn;
+    pb = boundary_list.head;
+    while(pb){
+      nnods_bc += ((boundary_t*)pb->data)->Nods.sizelist;
+      pb=pb->next;
+    }
+    aux_nod = malloc(nnods_bc*sizeof(int));
+
+    i=0;
+    pb = boundary_list.head;
+    while(pb){
+      pn = ((boundary_t*)pb->data)->Nods.head;
+      while(pn){
+	n_orig = *(int*)pn->data;
+	p = bsearch(&n_orig, mynods, nmynods, sizeof(int), cmpfunc); 
+	if(!p){
+	  PetscPrintf(MICRO_COMM,
+	      "A boundary node (%d) seems now to not belong to this process (rank:%d)",n_orig,rank_mic);
+	  return 1;
+	}
+	n_loc = p - mynods;       // Local numeration
+	n_glo = loc2petsc[n_loc]; // PETSc numeration
+	aux_nod[i]  = n_glo;
+	i++;
+	pn=pn->next;
+      }
+      pb=pb->next;
+    }
+
+    /*
+       we order the vector of boundary nodes in order to delete those 
+       nodes which are repeated
+     */
+    qsort(aux_nod, nnods_bc, sizeof(int), cmpfunc);
+    for(i=0;i<nnods_bc;i++){
+      if(i!=0){
+	if(aux_nod[i]!=aux_nod[i-1]){
+	  nnods_bc_a++;
+	}
+      }
+      else if(i==0){
+	nnods_bc_a=1;
+      }
+    }
+
+    ((homog_ld_t*)homo.st)->nnods_bc = nnods_bc_a;
+    ((homog_ld_t*)homo.st)->index  = malloc(nnods_bc_a*dim*sizeof(int));
+    ((homog_ld_t*)homo.st)->ub_val = malloc(nnods_bc_a*dim*sizeof(double));
+
+    c = 0;
+    for(i=0;i<nnods_bc;i++){
+      if(i!=0){
+	if(aux_nod[i]!=aux_nod[i-1]){
+	  for(d=0;d<dim;d++){
+	    ((homog_ld_t*)homo.st)->index[c*dim+d] = aux_nod[i]*dim + d;
+	  }
+	  c++;
+	}
+      }
+      else if(i==0){
+	for(d=0;d<dim;d++){
+	  ((homog_ld_t*)homo.st)->index[c*dim+d] = aux_nod[i]*dim + d;
+	}
+	c++;
+      }
+    }
+
+  }
   return 0;
 }
 /****************************************************************************************************/
+int micro_homogenize_linear_hexa(MPI_Comm MICRO_COMM, double strain_mac[6], double strain_ave[6], double stress_ave[6])
+{
+
+  /*
+     Performs linear homogenization of the RVE cell acording to <strain_mac[6]>
+   */
+
+  int ierr, nr_its = -1; 
+  //  int kspits = -1;
+  double norm = -1.0, norm_tol = 1.0e-8, max_its = 3; 
+  //  double ksp_norm = -1.0;
+
+  ierr = micro_apply_bc_linear_hexa(strain_mac, &x, &A, &b, SET_DISPLACE);CHKERRQ(ierr);
+
+  while( nr_its < max_its && norm > norm_tol )
+  {
+    ierr = assembly_residual_sd( &x, &b);CHKERRQ(ierr);
+    ierr = micro_apply_bc_linear_hexa(strain_mac, &x, &A, &b, SET_RESIDUAL);CHKERRQ(ierr);
+    ierr = VecNorm(b,NORM_2,&norm);CHKERRQ(ierr);
+    if( !(norm > norm_tol) )break;
+    ierr = VecScale(b,-1.0); CHKERRQ(ierr);
+    ierr = assembly_jacobian_sd(&A);
+    ierr = micro_apply_bc_linear_hexa(strain_mac, &x, &A, &b, SET_JACOBIAN);CHKERRQ(ierr);
+    ierr = KSPSolve(ksp,b,dx);CHKERRQ(ierr);
+    ierr = VecAXPY( x, 1.0, dx); CHKERRQ(ierr);
+    nr_its ++;
+  }
+
+  ierr = calc_ave_strain_stress(MICRO_COMM, &x, strain_ave, stress_ave);CHKERRQ(ierr);
+
+  return 0;
+}
