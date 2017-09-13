4a00126b39538c802dec080a5813da8c61501f5a
GG1991 <giuntoli1991@gmail.com>
2017-09-13 17:01:03 +0200

    adding a case to test LD BC fiber deviaton from center

--- exs/ex1/mesh_converg.sh
+++ exs/ex1/mesh_converg.sh
@@ -6,12 +6,12 @@
 
 NM=1
 
-if [ -e "convergence_const.dat" ]; then
-   rm convergence_const.dat
+if [ -e "conv_c.dat" ]; then
+   rm conv_c.dat
 fi
 
-if [ -e "convergence_stress.dat" ]; then
-   rm convergence_stress.dat
+if [ -e "conv_stress.dat" ]; then
+   rm conv_stress.dat
 fi
 
 function cube_2d {
@@ -20,7 +20,7 @@ sizes=(3 4 5 6 7 8 9 10 15 20 25 30 35 40 45 50 60 65 70 75 80)
 for i in ${sizes[@]};
 do
 
-  m4 -DN_m4=$i $mesh_folder/cube_2d_analysis.geo.m4 > cube_2d_analysis.geo
+  m4 -DN_m4=$i -Dlc_m4=1.5 $mesh_folder/cube_2d_analysis.geo.m4 > cube_2d_analysis.geo
   gmsh -2 cube_2d_analysis.geo
   rm cube_2d_analysis.geo
 
@@ -38,9 +38,9 @@ do
     awk -v i_awk=$i '
     /Constitutive/ {
       printf "%d ",i_awk;
-      for(j=0;j<6;j++){
+      for(j=0;j<3;j++){
 	getline;
-	for(i=1;i<=6;i++){
+	for(i=1;i<=3;i++){
 	  printf "%e ",$i;
 	}
       }
@@ -50,7 +50,7 @@ do
     awk -v i_awk=$i '
     /stress_ave/ {
       printf "%d ",i_awk;
-      for(i=1;i<=6;i++){
+      for(i=1;i<=3;i++){
 	printf "%e ",$(i+2);
       }
       printf "\n";
@@ -150,6 +150,67 @@ done
 
 }
 
+#
+# Creates an NX x NX struct mesh 
+# simulates an array of fibers
+# is for analize the dependence of the 
+# fiber position inside the RVE 
+# using LD BC
+#
+function struc_array {
+
+mesh_folder="../../meshes/cube_unif"
+LX=1.5
+NX=30
+for i in $(seq 1 3);
+do
+
+  lx=$(echo "$LX * $i" | bc)
+  nx=$(echo "$NX * $i" | bc)
+  
+  m4 -DN_m4=$nx -Dlc_m4=$lx $mesh_folder/cube_2d_analysis.geo.m4 > cube_2d_analysis.geo
+  gmsh -2 cube_2d_analysis.geo
+  rm cube_2d_analysis.geo
+
+  ./mpirun -np $NM ../../micro/micro \
+    -input ex1_2d.spu \
+    -mesh cube_2d_analysis.msh \
+    -dim 2 \
+    -mesh_gmsh \
+    -pc_type lu \
+    -options_left 0 \
+    -homo_ld \
+    -fiber_cilin 0.4,-0.4,+0.4 \
+    -fiber_nx $i \
+    -fiber_ny $i \
+    -print_vtu > micro.out
+
+    awk -v i_awk=$i '
+    /Constitutive/ {
+      printf "%d ",i_awk;
+      for(j=0;j<3;j++){
+	getline;
+	for(i=1;i<=3;i++){
+	  printf "%e ",$i;
+	}
+      }
+      printf "\n";
+    }' micro.out >> conv_c.dat
+
+    awk -v i_awk=$i '
+    /stress_ave/ {
+      printf "%d ",i_awk;
+      for(i=1;i<=3;i++){
+	printf "%e ",$(i+2);
+      }
+      printf "\n";
+      exit;
+    }' micro.out >> conv_stress.dat
+
+done
+
+}
 #cube_2d
 #cube_fiber_2d
 #rve_size_fiber_2d
+#struc_array
