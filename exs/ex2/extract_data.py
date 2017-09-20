76ccb8368c346b403c7d34848806146eba176e94
GG1991 <giuntoli1991@gmail.com>
2017-09-20 20:35:48 +0200

    adding stuff to python script

--- exs/ex2/extract_data.py
+++ exs/ex2/extract_data.py
@@ -60,11 +60,161 @@ def getPtsCoord( _Obj, _T ):
       Pts.append( coord )
 
     return np.array(Pts)
+#----------------------------------------------------------------# 
+def conv_macro():
 
+    mesh_size=[5, 10, 15, 20, 25, 30, 35, 40]
+    integrals = np.zeros( (len(mesh_size),2) )
+    
+    for method in methods:
+      n = 0
+      for i in mesh_size:
+         print 'case ', i,'x',i
+         os.system('m4 -DNx_m4_m4='+`i`+' -Dhomo_m4='+`method`+' run_multiscale_analysis.sh.m4 > run_multiscale_analysis.sh')
+         os.system("chmod 755 run_multiscale_analysis.sh ")
+         os.system("./run_multiscale_analysis.sh > macro.out")
+         fin   = "macro_t_1.pvtu"
+         PVTU  = XMLPartitionedUnstructuredGridReader(FileName=fin)
+         Times = np.array(PVTU.TimestepValues)
+      
+         PlotOverLine3 = PlotOverLine( Input=PVTU, guiName="PlotOverLine3", Source="High Resolution Line Source" )
+         PlotOverLine3.Source.Point2 = [30.0, 15.0, 0.0]
+         PlotOverLine3.Source.Point1 = [0.0 , 15.0, 0.0]
+         PlotOverLine3.Source.Resolution = 1000
+         PlotOverLine3.UpdatePipeline()
+      
+         key   = "energy"
+         Ene   = getPtsData( PlotOverLine3, key )
+      
+         key = "arc_length"
+         leng = getPtsData( PlotOverLine3, key )
+      
+         Ene_int = np.trapz(Ene,leng)
+         integrals[n,0] = i
+         integrals[n,1] = Ene_int
+         n += 1
+    
+      np.savetxt('integrals_'+method+'.dat', integrals )
+
+#----------------------------------------------------------------# 
+def conv_rve():
+
+    rve_size=[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    integrals = np.zeros( (len(rve_size),2) )
+     
+    n = 0
+    for i in rve_size:
+      print 'case ', i
+      os.system('m4 -DNx_m4_m4=11 -Dhomo_m4=homo_unif_strains run_multiscale_analysis.sh.m4 > run_multiscale_analysis.sh')
+      os.system("chmod 755 run_multiscale_analysis.sh ")
+      os.system('m4 -DNint_m4='+`i`+' ../../meshes/cube_fiber/cube_fiber_2d_analysis.geo.m4 > cube_fiber_2d_analysis.geo')
+      os.system('gmsh -2 cube_fiber_2d_analysis.geo')
+      os.system('mv cube_fiber_2d_analysis.msh cube_fiber_2d.msh')
+
+      os.system("./run_multiscale_analysis.sh > macro.out")
+      fin   = "macro_t_1.pvtu"
+      PVTU  = XMLPartitionedUnstructuredGridReader(FileName=fin)
+      Times = np.array(PVTU.TimestepValues)
+  
+      PlotOverLine3 = PlotOverLine( Input=PVTU, guiName="PlotOverLine3", Source="High Resolution Line Source" )
+      PlotOverLine3.Source.Point2 = [30.0, 15.0, 0.0]
+      PlotOverLine3.Source.Point1 = [0.0 , 15.0, 0.0]
+      PlotOverLine3.Source.Resolution = 1000
+      PlotOverLine3.UpdatePipeline()
+  
+      key   = "energy"
+      Ene   = getPtsData( PlotOverLine3, key )
+  
+      key = "arc_length"
+      leng = getPtsData( PlotOverLine3, key )
+  
+      Ene_int = np.trapz(Ene,leng)
+      integrals[n,0] = i
+      integrals[n,1] = Ene_int
+      n += 1
+  
+    np.savetxt('integrals_rve_convergence.dat', integrals )
+
+#----------------------------------------------------------------# 
+def rve_shape_analysis():
+
+    rve_size=[1, 2, 3, 4, 5, 6, 7, 8]
+    integrals = np.zeros( (len(rve_size),2) )
+     
+    n = 0
+    for i in rve_size:
+      print 'case ', i
+      os.system('m4 -DNx_m4_m4=21 -Dhomo_m4=homo_unif_strains run_multiscale_analysis.sh.m4 > run_multiscale_analysis.sh')
+      os.system("chmod 755 run_multiscale_analysis.sh ")
+      os.system('cp ../../meshes/cube_fiber/struct_fiber_2d_'+`i`+'_'+`i`+'.msh cube_fiber_2d.msh')
+
+      os.system('./run_multiscale_analysis.sh > macro_'+`i`+'.out')
+      fin   = "macro_t_1.pvtu"
+      PVTU  = XMLPartitionedUnstructuredGridReader(FileName=fin)
+      Times = np.array(PVTU.TimestepValues)
+  
+      PlotOverLine3 = PlotOverLine( Input=PVTU, guiName="PlotOverLine3", Source="High Resolution Line Source" )
+      PlotOverLine3.Source.Point2 = [30.0, 15.0, 0.0]
+      PlotOverLine3.Source.Point1 = [0.0 , 15.0, 0.0]
+      PlotOverLine3.Source.Resolution = 1000
+      PlotOverLine3.UpdatePipeline()
+  
+      key   = "energy"
+      Ene   = getPtsData( PlotOverLine3, key )
+  
+      key = "arc_length"
+      leng = getPtsData( PlotOverLine3, key )
+  
+      aux = np.zeros( (leng.shape[0],2) )
+      aux[:,1] = Ene[:]
+      aux[:,0] = leng 
+
+      np.savetxt('rve_shape_analysis_'+`i`+'.dat', aux )
+      n += 1
+
+#----------------------------------------------------------------# 
+def rve_shape_analysis_1():
+
+    rve_size=[1,2,3]
+    integrals = np.zeros( (len(rve_size),2) )
+     
+    n = 0
+    for i in rve_size:
+      print 'case ', i
+      os.system('m4 -Dlc_m4_m4='+`i*1.5`+' -DN_m4_m4='+`i*71`+' -Dnx_m4='+`i`+' run_multiscale_analysis_struc_micro.sh.m4 > run_multiscale_analysis.sh')
+      os.system("chmod 755 run_multiscale_analysis.sh ")
+
+      os.system('./run_multiscale_analysis.sh > macro_'+`i`+'.out')
+      fin   = "macro_t_1.pvtu"
+      PVTU  = XMLPartitionedUnstructuredGridReader(FileName=fin)
+      Times = np.array(PVTU.TimestepValues)
+  
+      PlotOverLine3 = PlotOverLine( Input=PVTU, guiName="PlotOverLine3", Source="High Resolution Line Source" )
+      PlotOverLine3.Source.Point2 = [30.0, 15.0, 0.0]
+      PlotOverLine3.Source.Point1 = [0.0 , 15.0, 0.0]
+      PlotOverLine3.Source.Resolution = 1000
+      PlotOverLine3.UpdatePipeline()
+  
+      key   = "energy"
+      Ene   = getPtsData( PlotOverLine3, key )
+  
+      key = "arc_length"
+      leng = getPtsData( PlotOverLine3, key )
+  
+      aux = np.zeros( (leng.shape[0],2) )
+      aux[:,1] = Ene[:]
+      aux[:,0] = leng 
+
+      np.savetxt('rve_shape_analysis_1_'+`i`+'.dat', aux )
+      n += 1
+  
 #----------------------------------------------------------------# 
 fin   = "direct/macro_t_1.pvtu"
 PVTU  = XMLPartitionedUnstructuredGridReader(FileName=fin)
 KEYs  = PVTU.GetPointDataInformation().keys()
+print KEYs
+KEYs  = PVTU.GetCellDataInformation().keys()
+print KEYs
 Times = np.array(PVTU.TimestepValues)
 
 PlotOverLine1 = PlotOverLine( Input=PVTU, guiName="PlotOverLine1", Source="High Resolution Line Source" )
@@ -79,28 +229,33 @@ Res   = getPtsData( PlotOverLine1, key )
 key   = "displ"
 Displ = getPtsData( PlotOverLine1, key )
 
+key     = "energy_interp"
+Ene_i   = getPtsData( PlotOverLine1, key )
+
 key   = "energy"
 Ene   = getPtsData( PlotOverLine1, key )
 
 key = "arc_length"
 leng = getPtsData( PlotOverLine1, key )
 
-aux = np.zeros( (leng.shape[0],4) )
+aux = np.zeros( (leng.shape[0],5) )
+aux[:,4] = Ene_i[:]
 aux[:,3] = Ene[:]
 aux[:,2] = Res[:,0]
 aux[:,1] = Displ[:,0]
 aux[:,0] = leng 
 
 np.savetxt("direct.dat", aux )
+Ene_int_direct = np.trapz(Ene_i,leng)
 
 fin   = "unif_strains/macro_t_1.pvtu"
 PVTU  = XMLPartitionedUnstructuredGridReader(FileName=fin)
-KEYs  = PVTU.GetPointDataInformation().keys()
 Times = np.array(PVTU.TimestepValues)
 
 PlotOverLine2 = PlotOverLine( Input=PVTU, guiName="PlotOverLine2", Source="High Resolution Line Source" )
 PlotOverLine2.Source.Point2 = [30.0, 15.0, 0.0]
 PlotOverLine2.Source.Point1 = [0.0 , 15.0, 0.0]
+PlotOverLine2.Source.Resolution = 1000
 PlotOverLine2.UpdatePipeline()
 
 key   = "residual"
@@ -122,3 +277,45 @@ aux[:,1] = Displ[:,0]
 aux[:,0] = leng 
 
 np.savetxt("unif_strains.dat", aux )
+Ene_int_unif_strains = np.trapz(Ene,leng)
+
+fin   = "taylor/macro_t_1.pvtu"
+PVTU  = XMLPartitionedUnstructuredGridReader(FileName=fin)
+Times = np.array(PVTU.TimestepValues)
+
+PlotOverLine3 = PlotOverLine( Input=PVTU, guiName="PlotOverLine3", Source="High Resolution Line Source" )
+PlotOverLine3.Source.Point2 = [30.0, 15.0, 0.0]
+PlotOverLine3.Source.Point1 = [0.0 , 15.0, 0.0]
+PlotOverLine3.Source.Resolution = 1000
+PlotOverLine3.UpdatePipeline()
+
+key   = "residual"
+Res   = getPtsData( PlotOverLine3, key )
+
+key   = "displ"
+Displ = getPtsData( PlotOverLine3, key )
+
+key   = "energy"
+Ene   = getPtsData( PlotOverLine3, key )
+
+key = "arc_length"
+leng = getPtsData( PlotOverLine3, key )
+
+aux = np.zeros( (leng.shape[0],4) )
+aux[:,3] = Ene[:]
+aux[:,2] = Res[:,0]
+aux[:,1] = Displ[:,0]
+aux[:,0] = leng 
+
+np.savetxt("taylor.dat", aux )
+
+Ene_int_taylor = np.trapz(Ene,leng)
+print "Ene_int_taylor       = ", Ene_int_taylor
+print "Ene_int_unif_strains = ", Ene_int_unif_strains
+print "Ene_int_taylor       = ", Ene_int_direct
+
+
+#conv_macro()
+#conv_rve()
+#rve_shape_analysis()
+rve_shape_analysis_1()
