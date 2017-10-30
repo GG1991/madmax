d1cce1472481db03bf9414c12d6a06910844e8d3
GG1991 <giuntoli1991@gmail.com>
2017-10-30 15:44:34 +0100

    changes on run_1 in exs/micro_hom

--- exs/micro_hom/run_1.sh
+++ exs/micro_hom/run_1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./mpiexec -np 1 xterm -e gdb -x file.gdb --args  ../../micro/micro \
+./mpiexec -np 1 ../../micro/micro \
     -dim 2 \
-    -struct_n    10,10 \
+    -struct_n    3,3 \
     -struct_l    0.1,0.1 \
     -fiber_cilin 0.4,0.0,0.0,0.0   \
     -mat_fiber_t0  1.0e6,1.0e7,0.3 \
@@ -13,3 +13,4 @@
     -options_left 0
 
 #xterm -e gdb --args 
+#xterm -e gdb -x file.gdb --args 
