f83200e9f623005a6550b4d625bac34c351950fe
GG1991 <giuntoli1991@gmail.com>
2017-11-06 09:33:00 +0100

    homogenized editet

--- micro/src/mic_homogenize.c
+++ micro/src/mic_homogenize.c
@@ -23,227 +23,126 @@ int get_node_ghost_coor( int n , double * coord );
 int get_local_elem_node( int e , int * n_loc );
 int local_to_global_index( int local );
 int init_shapes(double ***sh, double ****dsh, double **wp, double *h, int dim);
+int vtkcode(int dim,int npe);
 
 int mic_homogenize_taylor(MPI_Comm MICRO_COMM, double strain_mac[6], double strain_ave[6], double stress_ave[6])
 {
-  /* 
-     This work only if inclusion names starts with FIBER and matrix name start with MATRIX
-     we assume only on kind of them 
-    
-     This kind of homogenization do not need to perform 3 (dim=2) or 6 (dim=3) experiments
-  */
-  int          i, j, e, ierr;
-  double       c_i[6][6];
-  double       c_m[6][6];
-  double       c[6][6];
-  double       vol_i = 0.0, vol_ia = 0.0;  // inclusion volume
-  double       vol_m = 0.0, vol_ma = 0.0;  // matrix volume
-  double       vol_t = 0.0;  // total volume
-  double       vol_e = 0.0;  // element volume
-  material_t   *mat = NULL; 
-
-  if(first_time_homo){
-    for(e=0;e<nelm;e++){
-
-      ierr = get_mat_from_elem(e, &mat);
-      ierr = get_elem_vol(e, &vol_e);
-      if(!strncmp(mat->name,"FIBER",5)){
-	vol_ia += vol_e;
-      }
-      else if(!strncmp(mat->name,"MATRIX",6)){
-	vol_ma += vol_e;
-      }
-      else{
-	PetscPrintf(MICRO_COMM,	"not possible to compute a Taylor homogenization with a material of type\n",mat->name);
-	return 1;
-      }
-
-    }
-    ierr = MPI_Allreduce(&vol_ia, &vol_i, 1, MPI_DOUBLE, MPI_SUM, MICRO_COMM);CHKERRQ(ierr);
-    ierr = MPI_Allreduce(&vol_ma, &vol_m, 1, MPI_DOUBLE, MPI_SUM, MICRO_COMM);CHKERRQ(ierr);
-    vol_t = vol_i + vol_m;
-    vi = vol_i / vol_t; // inclusion fraction
-    vm = vol_m / vol_t; // matrix fraction
-  }
-  get_c("FIBER" , 0, 0, strain_mac, c_i);  //returns c_i of FIBER
-  get_c("MATRIX", 0, 0, strain_mac, c_m);  //returns c_m of MATRIX
- 
-  if(homo_type==TAYLOR_P){
-    for(i=0;i<nvoi;i++){
-      for(j=0;j<nvoi;j++){
-	c[i][j] = vi * c_i[i][j] + vm * c_m[i][j];
-      }
-    }
-  }
-  else if(homo_type==TAYLOR_S){
-
-    int              s;
-    double           c_ia[36], c_ma[36]; // matrices in array
-    double           c_ii[36], c_mi[36]; // inverted matrices
-    double           c_a[36] , c_ai[36];
-    gsl_matrix_view  m, mi;
-    gsl_permutation  *p;
-
-    p  = gsl_permutation_alloc(nvoi);
-
-    for(i = 0; i < nvoi; ++i){
-      for(j = 0; j < nvoi; ++j){
-	c_ia[i*nvoi + j] = c_i[i][j];
-	c_ma[i*nvoi + j] = c_m[i][j];
-      }
-    }
-    
-    m  = gsl_matrix_view_array(c_ia,nvoi,nvoi);
-    mi = gsl_matrix_view_array(c_ii,nvoi,nvoi);
-    
-    gsl_linalg_LU_decomp (&m.matrix, p, &s);    
-    gsl_linalg_LU_invert (&m.matrix, p, &mi.matrix);
-    
-    for(i = 0; i < nvoi; ++i){
-      for(j = 0; j < nvoi; ++j){
-	c_ii[i*nvoi+j] = gsl_matrix_get(&mi.matrix,i,j);
-      }
-    }
-
-    m  = gsl_matrix_view_array(c_ma,nvoi,nvoi);
-    mi = gsl_matrix_view_array(c_mi,nvoi,nvoi);
-    
-    gsl_linalg_LU_decomp (&m.matrix, p, &s);    
-    gsl_linalg_LU_invert (&m.matrix, p, &mi.matrix);
-    
-    for(i = 0; i < nvoi; ++i){
-      for(j = 0; j < nvoi; ++j){
-	c_mi[i*nvoi+j] = gsl_matrix_get(&mi.matrix,i,j);
-      }
-    }
-    for(i = 0; i < nvoi*nvoi; ++i){
-      c_a[i] = vi * c_ii[i] + vm * c_mi[i];
-    }
-
-    m  = gsl_matrix_view_array(c_a ,nvoi,nvoi);
-    mi = gsl_matrix_view_array(c_ai,nvoi,nvoi);
-    
-    gsl_linalg_LU_decomp (&m.matrix, p, &s);    
-    gsl_linalg_LU_invert (&m.matrix, p, &mi.matrix);
-    
-    for(i = 0; i < nvoi; ++i){
-      for(j = 0; j < nvoi; ++j){
-	c[i][j] = gsl_matrix_get(&mi.matrix,i,j);
-      }
-    }
-     
-    gsl_permutation_free (p);
-  }
-  for(i=0;i<nvoi;i++){
-    strain_ave[i] = strain_mac[i];
-    stress_ave[i] = 0.0;
-    for(j=0;j<nvoi;j++){
-      stress_ave[i] += c[i][j] * strain_mac[j];
-    }
-  }
-
-  return 0;
-}
-/****************************************************************************************************/
-int mic_homogenize_unif_strains(MPI_Comm MICRO_COMM, double strain_mac[6], double strain_ave[6], double stress_ave[6])
-{
-  /*
-     ub = E . x
-  */
-
-  int    ierr, nr_its=-1;
-  int    nnods_bc, *nods_bc;
-  int    i, d, k;
-  int    *ix_loc, *ix_glo;
-  double *ub;
-  double norm=2*nr_norm_tol;
-  double strain_matrix[3][3];
-  double *x_arr, *b_arr;
-
-  if(dim==2){
-    strain_matrix[0][0]=strain_mac[0]    ; strain_matrix[0][1]=strain_mac[2]*0.5;
-    strain_matrix[1][0]=strain_mac[2]*0.5; strain_matrix[1][1]=strain_mac[1]    ;
-  }
-  else if(dim==3){
-    strain_matrix[0][0]=strain_mac[0]; strain_matrix[0][1]=strain_mac[3]; strain_matrix[0][2]=strain_mac[5];
-    strain_matrix[1][0]=strain_mac[3]; strain_matrix[1][1]=strain_mac[1]; strain_matrix[1][2]=strain_mac[4];
-    strain_matrix[2][0]=strain_mac[5]; strain_matrix[2][1]=strain_mac[4]; strain_matrix[2][2]=strain_mac[2];
-  }
-
-  /* get array of nods in al boundary without repetition */
-  ierr = get_nods_bc( &nods_bc, &nnods_bc);
-  if(ierr){
-    return 1;
-  }
-  ix_loc = malloc(nnods_bc*dim*sizeof(int));    /* indeces to search for local coordinates */
-  ix_glo = malloc(nnods_bc*dim*sizeof(int));    /* indeces to be set at boundary */
-  ub     = malloc(nnods_bc*dim*sizeof(double)); /* values  to be set at boundary */
-  ierr   = get_nods_index( nods_bc, nnods_bc, ix_loc, ix_glo);
-
-  // first we fill the value ub_val in <homog_ld_lagran_t> structure
-  for(i=0; i<nnods_bc; i++){
-    for(d=0;d<dim;d++){
-      ub[i*dim+d] = 0.0;
-      for(k=0;k<dim;k++){
-	ub[ i * dim + d] = ub[ i * dim + d] +  strain_matrix[d][k] * coord[ix_loc[i* dim + k]];
-      }
-    }
-  }
-
-  // first we fill the value ub_val in <homog_ld_lagran_t> structure
-  ierr = VecGetArray(x, &x_arr);CHKERRQ(ierr);
-  for(i=0; i<nnods_bc; i++){
-    for(d=0;d<dim;d++){
-      x_arr[ix_loc[i*dim+d]] = ub[i*dim+d]; 
-    }
-  }
-  ierr = VecRestoreArray(x, &x_arr);CHKERRQ(ierr);
-
-  while( nr_its < nr_max_its && norm > nr_norm_tol )
-  {
-    ierr = assembly_residual_sd(&x, &b);CHKERRQ(ierr);
-    ierr = VecGetArray(b, &b_arr);CHKERRQ(ierr);
-    for(i=0; i<nnods_bc; i++){
-      for(d=0;d<dim;d++){
-	b_arr[ix_loc[i*dim+d]] = 0.0;
-      }
-    }
-    ierr = VecRestoreArray(b, &b_arr);CHKERRQ(ierr);
-    ierr = VecScale(b,-1.0); CHKERRQ(ierr);
-
-    ierr = VecNorm(b,NORM_2,&norm);CHKERRQ(ierr);
-    if(!flag_coupling)
-      PetscPrintf(MICRO_COMM,"|b| = %lf \n",norm);
-
-    if( !(norm > nr_norm_tol) )break;
-
-    if(first_time_homo || !flag_linear_micro){
-      ierr = assembly_jacobian_sd(&A);
-    }
-    ierr = MatZeroRowsColumns(A, nnods_bc*dim, ix_glo, 1.0, NULL, NULL); CHKERRQ(ierr);
-    ierr = KSPSolve(ksp,b,dx);CHKERRQ(ierr);
-
-    ierr = VecAXPY( x, 1.0, dx); CHKERRQ(ierr);
-    ierr = MatMult(A, dx, b1);CHKERRQ(ierr);
-    
-    if( flag_print & (1<<PRINT_PETSC) ){
-      ierr = PetscViewerASCIIOpen(MICRO_COMM,"A.dat",&viewer); CHKERRQ(ierr);
-      ierr = MatView(A,viewer); CHKERRQ(ierr);
-      ierr = PetscViewerASCIIOpen(MICRO_COMM,"b.dat",&viewer); CHKERRQ(ierr);
-      ierr = VecView(b,viewer); CHKERRQ(ierr);
-      ierr = PetscViewerASCIIOpen(MICRO_COMM,"b1.dat",&viewer); CHKERRQ(ierr);
-      ierr = VecView(b1,viewer); CHKERRQ(ierr);
-      ierr = PetscViewerASCIIOpen(MICRO_COMM,"x.dat",&viewer); CHKERRQ(ierr);
-      ierr = VecView(x,viewer); CHKERRQ(ierr);
-      ierr = PetscViewerASCIIOpen(MICRO_COMM,"dx.dat",&viewer); CHKERRQ(ierr);
-      ierr = VecView(x,viewer); CHKERRQ(ierr);
-    }
-
-    nr_its ++;
-  }
-
-  ierr = calc_ave_strain_stress(MICRO_COMM, &x, strain_ave, stress_ave);CHKERRQ(ierr);
-
+//  /* 
+//     This work only if inclusion names starts with FIBER and matrix name start with MATRIX
+//     we assume only on kind of them 
+//    
+//     This kind of homogenization do not need to perform 3 (dim=2) or 6 (dim=3) experiments
+//  */
+//  int          i, j, e, ierr;
+//  double       c_i[6][6];
+//  double       c_m[6][6];
+//  double       c[6][6];
+//  double       vol_i = 0.0, vol_ia = 0.0;  // inclusion volume
+//  double       vol_m = 0.0, vol_ma = 0.0;  // matrix volume
+//  double       vol_t = 0.0;  // total volume
+//  double       vol_e = 0.0;  // element volume
+//  material_t   *mat = NULL; 
+//
+//  if(first_time_homo){
+//    for(e=0;e<nelm;e++){
+//
+//      ierr = get_mat_from_elem(e, &mat);
+//      ierr = get_elem_vol(e, &vol_e);
+//      if(!strncmp(mat->name,"FIBER",5)){
+//	vol_ia += vol_e;
+//      }
+//      else if(!strncmp(mat->name,"MATRIX",6)){
+//	vol_ma += vol_e;
+//      }
+//      else{
+//	PetscPrintf(MICRO_COMM,	"not possible to compute a Taylor homogenization with a material of type\n",mat->name);
+//	return 1;
+//      }
+//
+//    }
+//    ierr = MPI_Allreduce(&vol_ia, &vol_i, 1, MPI_DOUBLE, MPI_SUM, MICRO_COMM);CHKERRQ(ierr);
+//    ierr = MPI_Allreduce(&vol_ma, &vol_m, 1, MPI_DOUBLE, MPI_SUM, MICRO_COMM);CHKERRQ(ierr);
+//    vol_t = vol_i + vol_m;
+//    vi = vol_i / vol_t; // inclusion fraction
+//    vm = vol_m / vol_t; // matrix fraction
+//  }
+//  get_c("FIBER" , 0, 0, strain_mac, c_i);  //returns c_i of FIBER
+//  get_c("MATRIX", 0, 0, strain_mac, c_m);  //returns c_m of MATRIX
+// 
+//  if(homo_type==TAYLOR_P){
+//    for(i=0;i<nvoi;i++){
+//      for(j=0;j<nvoi;j++){
+//	c[i][j] = vi * c_i[i][j] + vm * c_m[i][j];
+//      }
+//    }
+//  }
+//  else if(homo_type==TAYLOR_S){
+//
+//    int              s;
+//    double           c_ia[36], c_ma[36]; // matrices in array
+//    double           c_ii[36], c_mi[36]; // inverted matrices
+//    double           c_a[36] , c_ai[36];
+//    gsl_matrix_view  m, mi;
+//    gsl_permutation  *p;
+//
+//    p  = gsl_permutation_alloc(nvoi);
+//
+//    for(i = 0; i < nvoi; ++i){
+//      for(j = 0; j < nvoi; ++j){
+//	c_ia[i*nvoi + j] = c_i[i][j];
+//	c_ma[i*nvoi + j] = c_m[i][j];
+//      }
+//    }
+//    
+//    m  = gsl_matrix_view_array(c_ia,nvoi,nvoi);
+//    mi = gsl_matrix_view_array(c_ii,nvoi,nvoi);
+//    
+//    gsl_linalg_LU_decomp (&m.matrix, p, &s);    
+//    gsl_linalg_LU_invert (&m.matrix, p, &mi.matrix);
+//    
+//    for(i = 0; i < nvoi; ++i){
+//      for(j = 0; j < nvoi; ++j){
+//	c_ii[i*nvoi+j] = gsl_matrix_get(&mi.matrix,i,j);
+//      }
+//    }
+//
+//    m  = gsl_matrix_view_array(c_ma,nvoi,nvoi);
+//    mi = gsl_matrix_view_array(c_mi,nvoi,nvoi);
+//    
+//    gsl_linalg_LU_decomp (&m.matrix, p, &s);    
+//    gsl_linalg_LU_invert (&m.matrix, p, &mi.matrix);
+//    
+//    for(i = 0; i < nvoi; ++i){
+//      for(j = 0; j < nvoi; ++j){
+//	c_mi[i*nvoi+j] = gsl_matrix_get(&mi.matrix,i,j);
+//      }
+//    }
+//    for(i = 0; i < nvoi*nvoi; ++i){
+//      c_a[i] = vi * c_ii[i] + vm * c_mi[i];
+//    }
+//
+//    m  = gsl_matrix_view_array(c_a ,nvoi,nvoi);
+//    mi = gsl_matrix_view_array(c_ai,nvoi,nvoi);
+//    
+//    gsl_linalg_LU_decomp (&m.matrix, p, &s);    
+//    gsl_linalg_LU_invert (&m.matrix, p, &mi.matrix);
+//    
+//    for(i = 0; i < nvoi; ++i){
+//      for(j = 0; j < nvoi; ++j){
+//	c[i][j] = gsl_matrix_get(&mi.matrix,i,j);
+//      }
+//    }
+//     
+//    gsl_permutation_free (p);
+//  }
+//  for(i=0;i<nvoi;i++){
+//    strain_ave[i] = strain_mac[i];
+//    stress_ave[i] = 0.0;
+//    for(j=0;j<nvoi;j++){
+//      stress_ave[i] += c[i][j] * strain_mac[j];
+//    }
+//  }
+//
   return 0;
 }
 /****************************************************************************************************/
@@ -963,7 +862,7 @@ int local_to_global_index( int local )
 
   if( dim == 2 ){
 
-    return ny_inf*nx*dim + local;
+    return ny_inf * nx * dim + local;
 
   }
 
@@ -1522,3 +1421,48 @@ int init_shapes(double ***sh, double ****dsh, double **wp, double *h, int dim)
 
   return 0;
 }
+/****************************************************************************************************/
+#define   VTK_POINT         1
+#define   VTK_LINE          3
+#define   VTK_TRIANGLE      5
+#define   VTK_QUADRANGLE    9
+#define   VTK_TETRAHEDRON   10
+#define   VTK_HEXAHEDRON    12
+#define   VTK_6N_PRISM      13
+
+int vtkcode(int dim,int npe)
+{
+
+  switch(dim){
+    case 1:
+      switch(npe){
+        case 2 :
+          return VTK_LINE;
+        default:
+          return -1;
+      }
+    case 2:
+      switch(npe){
+        case 3 :
+          return VTK_TRIANGLE;
+        case 4 :
+          return VTK_QUADRANGLE;
+        default:
+          return -1;
+      }
+    case 3:
+      switch(npe){
+        case 4 :
+          return VTK_TETRAHEDRON;
+        case 6 :
+          return VTK_6N_PRISM;  
+        case 8 :
+          return VTK_HEXAHEDRON;  
+        default:
+          return -1;
+      }
+    default:
+      return -1;
+  }
+}
+/****************************************************************************************************/
