71d53dd33f5f369d5edf86031205a35d8a544f46
GG1991 <giuntoli1991@gmail.com>
2017-11-07 14:38:08 +0100

    code for profiling

---
+++ inc/trace.h
@@ -0,0 +1,19 @@
+#include <mpi.h>
+
+#ifndef _TRACE_H_
+#define _TRACE_H_
+
+typedef struct _trace_t{
+
+  FILE   *file;
+
+}trace_t;
+
+
+trace_t trace;
+
+int init_trace( MPI_Comm COMM, const char *file_name );
+int save_event( MPI_Comm COMM, const char *event );
+int end_trace ( MPI_Comm COMM );
+
+#endif
