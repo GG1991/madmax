5e4947d223b5c36ac6915289f24f99698545cee7
GG1991 <giuntoli1991@gmail.com>
2017-11-07 10:21:17 +0100

    taylor parallel seems to work

--- micro/src/mic_homogenize.c
+++ micro/src/mic_homogenize.c
@@ -28,122 +28,92 @@ int get_elem_type( int e , int *type );
 
 int mic_homogenize_taylor( MPI_Comm MICRO_COMM, double strain_mac[6], double strain_ave[6], double stress_ave[6] )
 {
-//  /* 
-//     This work only if inclusion names starts with FIBER and matrix name start with MATRIX
-//     we assume only on kind of them 
-//    
-//     This kind of homogenization do not need to perform 3 (dim=2) or 6 (dim=3) experiments
-//  */
-//  int          i, j, e, ierr;
-//  double       c_i[6][6];
-//  double       c_m[6][6];
-//  double       c[6][6];
-//  double       vol_i = 0.0, vol_ia = 0.0;  // inclusion volume
-//  double       vol_m = 0.0, vol_ma = 0.0;  // matrix volume
-//  double       vol_t = 0.0;  // total volume
-//  double       vol_e = 0.0;  // element volume
-//  material_t   *mat = NULL; 
-//
-//  if(first_time_homo){
-//    for(e=0;e<nelm;e++){
-//
-//      ierr = get_mat_from_elem(e, &mat);
-//      ierr = get_elem_vol(e, &vol_e);
-//      if(!strncmp(mat->name,"FIBER",5)){
-//	vol_ia += vol_e;
-//      }
-//      else if(!strncmp(mat->name,"MATRIX",6)){
-//	vol_ma += vol_e;
-//      }
-//      else{
-//	PetscPrintf(MICRO_COMM,	"not possible to compute a Taylor homogenization with a material of type\n",mat->name);
-//	return 1;
-//      }
-//
-//    }
-//    ierr = MPI_Allreduce(&vol_ia, &vol_i, 1, MPI_DOUBLE, MPI_SUM, MICRO_COMM);CHKERRQ(ierr);
-//    ierr = MPI_Allreduce(&vol_ma, &vol_m, 1, MPI_DOUBLE, MPI_SUM, MICRO_COMM);CHKERRQ(ierr);
-//    vol_t = vol_i + vol_m;
-//    vi = vol_i / vol_t; // inclusion fraction
-//    vm = vol_m / vol_t; // matrix fraction
-//  }
-//  get_c("FIBER" , 0, 0, strain_mac, c_i);  //returns c_i of FIBER
-//  get_c("MATRIX", 0, 0, strain_mac, c_m);  //returns c_m of MATRIX
-// 
-//  if(homo_type==TAYLOR_P){
-//    for(i=0;i<nvoi;i++){
-//      for(j=0;j<nvoi;j++){
-//	c[i][j] = vi * c_i[i][j] + vm * c_m[i][j];
-//      }
-//    }
-//  }
-//  else if(homo_type==TAYLOR_S){
-//
+  /* Mixture theory */
+    
+  int          i, j, e, ierr;
+  int          elem_type;
+  double       *c_i = malloc( nvoi*nvoi * sizeof(double));
+  double       *c_m = malloc( nvoi*nvoi * sizeof(double));
+  double       c[6][6];
+  double       vol_i = 0.0, vol_ia = 0.0;  // inclusion volume
+  double       vol_m = 0.0, vol_ma = 0.0;  // matrix volume
+
+  if(first_time_homo){
+
+    for( e = 0 ; e < nelm ; e++ ){
+
+      /* get element type */
+      get_elem_type( e, &elem_type );
+
+      if( elem_type == 1 )
+	vol_ia += vol_elem;
+      else
+	vol_ma += vol_elem;
+
+    }
+    ierr = MPI_Allreduce( &vol_ia, &vol_i, 1, MPI_DOUBLE, MPI_SUM, MICRO_COMM); if(ierr) return 1;
+    ierr = MPI_Allreduce( &vol_ma, &vol_m, 1, MPI_DOUBLE, MPI_SUM, MICRO_COMM); if(ierr) return 1;
+    
+    vi    = vol_i / vol_tot;                   // inclusion fraction
+    vm    = vol_m / vol_tot;                   // matrix fraction
+  }
+  get_c_tan("FIBER" , 0, 0, NULL, c_i);  //returns c_i of FIBER
+  get_c_tan("MATRIX", 0, 0, NULL, c_m);  //returns c_m of MATRIX
+ 
+  if( homo_type == TAYLOR_P ){
+
+    /* PARALLEL THEORY */
+    for( i = 0 ; i < nvoi ; i++ ){
+      for( j = 0 ; j < nvoi ; j++ )
+	c[i][j] = vi * c_i[i*nvoi + j] + vm * c_m[i*nvoi + j];
+    }
+
+  }
+  else if( homo_type == TAYLOR_S ){
+
+//    /* SERIAL THEORY */
 //    int              s;
-//    double           c_ia[36], c_ma[36]; // matrices in array
-//    double           c_ii[36], c_mi[36]; // inverted matrices
-//    double           c_a[36] , c_ai[36];
-//    gsl_matrix_view  m, mi;
+//    gsl_matrix_view  gsl_c_m , gsl_c_i;   // gsl arrays of c_i and c_m
+//    gsl_matrix_view  gsl_c_mi, gsl_c_ii;  // gsl arrays of c_i and c_m (inverted) (inverted)
 //    gsl_permutation  *p;
 //
 //    p  = gsl_permutation_alloc(nvoi);
 //
-//    for(i = 0; i < nvoi; ++i){
-//      for(j = 0; j < nvoi; ++j){
-//	c_ia[i*nvoi + j] = c_i[i][j];
-//	c_ma[i*nvoi + j] = c_m[i][j];
-//      }
-//    }
+//    gsl_c_i = gsl_matrix_view_array(c_i,nvoi,nvoi);
+//    gsl_c_m = gsl_matrix_view_array(c_m,nvoi,nvoi);
 //    
-//    m  = gsl_matrix_view_array(c_ia,nvoi,nvoi);
-//    mi = gsl_matrix_view_array(c_ii,nvoi,nvoi);
+//    gsl_linalg_LU_decomp (&gsl_c_m.matrix, p, &s);    
+//    gsl_linalg_LU_invert (&gsl_c_m.matrix, p, &gsl_c_mi.matrix);
+//    gsl_linalg_LU_decomp (&gsl_c_i.matrix, p, &s);    
+//    gsl_linalg_LU_invert (&gsl_c_i.matrix, p, &gsl_c_ii.matrix);
 //    
-//    gsl_linalg_LU_decomp (&m.matrix, p, &s);    
-//    gsl_linalg_LU_invert (&m.matrix, p, &mi.matrix);
-//    
-//    for(i = 0; i < nvoi; ++i){
-//      for(j = 0; j < nvoi; ++j){
-//	c_ii[i*nvoi+j] = gsl_matrix_get(&mi.matrix,i,j);
+//    for( i = 0; i < nvoi; i++ ){
+//      for( j = 0; j < nvoi; j++ ){
+//	c_m[i*nvoi+j] = gsl_matrix_get( &gsl_c_mi.matrix, i, j);
+//	c_i[i*nvoi+j] = gsl_matrix_get( &gsl_c_ii.matrix, i, j);
 //      }
 //    }
 //
-//    m  = gsl_matrix_view_array(c_ma,nvoi,nvoi);
-//    mi = gsl_matrix_view_array(c_mi,nvoi,nvoi);
-//    
-//    gsl_linalg_LU_decomp (&m.matrix, p, &s);    
-//    gsl_linalg_LU_invert (&m.matrix, p, &mi.matrix);
-//    
-//    for(i = 0; i < nvoi; ++i){
-//      for(j = 0; j < nvoi; ++j){
-//	c_mi[i*nvoi+j] = gsl_matrix_get(&mi.matrix,i,j);
-//      }
-//    }
-//    for(i = 0; i < nvoi*nvoi; ++i){
-//      c_a[i] = vi * c_ii[i] + vm * c_mi[i];
-//    }
+//    for( i = 0; i < nvoi*nvoi ; i++)
+//      c_i[i] = vi * c_i[i] + vm * c_m[i];
 //
-//    m  = gsl_matrix_view_array(c_a ,nvoi,nvoi);
-//    mi = gsl_matrix_view_array(c_ai,nvoi,nvoi);
-//    
 //    gsl_linalg_LU_decomp (&m.matrix, p, &s);    
 //    gsl_linalg_LU_invert (&m.matrix, p, &mi.matrix);
 //    
-//    for(i = 0; i < nvoi; ++i){
-//      for(j = 0; j < nvoi; ++j){
+//    for( i = 0; i < nvoi; i++ ){
+//      for( j = 0; j < nvoi; j++ )
 //	c[i][j] = gsl_matrix_get(&mi.matrix,i,j);
-//      }
 //    }
 //     
 //    gsl_permutation_free (p);
-//  }
-//  for(i=0;i<nvoi;i++){
-//    strain_ave[i] = strain_mac[i];
-//    stress_ave[i] = 0.0;
-//    for(j=0;j<nvoi;j++){
-//      stress_ave[i] += c[i][j] * strain_mac[j];
-//    }
-//  }
-//
+  }
+  for( i = 0; i < nvoi; i++ ){
+    strain_ave[i] = strain_mac[i];
+    stress_ave[i] = 0.0;
+    for( j = 0; j < nvoi; j++ )
+      stress_ave[i] += c[i][j] * strain_mac[j];
+  }
+
   return 0;
 }
 
