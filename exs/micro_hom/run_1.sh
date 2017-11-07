4e7c29ad2ae961fedbb50647eda79dcf946d5527
Guido Giuntoli <giuntoli1991@gmail.com>
2017-11-08 00:30:23 +0100

    scalabity script

--- exs/micro_hom/run_1.sh
+++ exs/micro_hom/run_1.sh
@@ -9,31 +9,32 @@ fi
 
 ./mpiexec -np $NP  ../../micro/micro \
     -dim 2 \
-    -struct_n    15,15 \
-    -struct_l    0.1,0.1 \
-    -fiber_cilin 0.4,0.0,0.0,0.0   \
+    -struct_n      50,50 \
+    -struct_l      0.1,0.1 \
+    -fiber_cilin   0.03,0.0,0.0,0.0   \
     -mat_fiber_t0  1.0e6,1.0e7,0.3 \
     -mat_matrix_t0 1.0e6,1.0e6,0.3 \
-    -pc_type lu \
+    -pc_type       jacobi \
+    -ksp_type      cg \
     -print_vtu \
-    -print_petsc \
     -homo_us \
-    -options_left 0 
+    -options_left 0
 #: -np 1 xterm -e gdb -x file.gdb --args ../../micro/micro \
-#   -dim 2 \
-#   -struct_n    3,3 \
-#   -struct_l    0.1,0.1 \
-#   -fiber_cilin 0.4,0.0,0.0,0.0   \
-#   -mat_fiber_t0  1.0e6,1.0e7,0.3 \
-#   -mat_matrix_t0 1.0e6,1.0e6,0.3 \
-#   -pc_type lu \
-#   -print_vtu \
-#   -homo_us \
-#   -options_left 0
+#    -dim 2 \
+#    -struct_n      15,15 \
+#    -struct_l      0.1,0.1 \
+#    -fiber_cilin   0.03,0.0,0.0,0.0   \
+#    -mat_fiber_t0  1.0e6,1.0e7,0.3 \
+#    -mat_matrix_t0 1.0e6,1.0e6,0.3 \
+#    -pc_type       jacobi \
+#    -ksp_type      cg \
+#    -print_vtu \
+#    -homo_us \
+#    -options_left 0
+#    -print_petsc \
 
 #xterm -e gdb --args 
 #xterm -e gdb -x file.gdb --args 
-#-pc_type jacobi \
 #-pc_type lu \
 #-pc_type  jacobi \
 #-ksp_type cg \
---
+++ exs/micro_hom/scalability.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -e "times.dat" ]
+then
+  rm times.dat
+fi
+
+echo "running"
+for i in $(seq 1 10) 
+do
+  echo "$i proc"
+  ./run_1.sh $i > out.dat
+  mv micro_trace.dat micro_trace_$i.dat
+  echo "$i $(sed '$!d' micro_trace_$i.dat | awk '{print $2}')" >> times.dat
+
+done
