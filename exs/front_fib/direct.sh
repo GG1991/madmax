69e6450eb8eef5dfbf162957fdf5456bd64328c3
GG1991 <giuntoli1991@gmail.com>
2017-11-15 15:40:01 +0100

    add direct.sh to simulate frontal fiber example by direct method

---
+++ exs/front_fib/direct.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+MPIEXEC="/home/guido/libs/openmpi-install/bin/mpiexec" 
+
+if [ $# -eq 0 ]; then
+  NP=1;
+else
+  NP=$1;
+fi
+
+#$MPIEXEC -np $NP xterm -e gdb -x file.gdb --args ../../macro/macro \
+$MPIEXEC -np $NP ../../macro/macro \
+    -boundary "X0 11 0 0","X1 11 1 0" \
+    -material "MATRIX TYPE_0 1.0e7 1.0e6 0.3","FIBER TYPE_0 1.0e8 1.0e6 0.3","MICRO TYPE_1" \
+    -function "0 2 0.0 0.0 1.0 0.0","1 2 0.0 0.0 1.0 0.001" \
+    -mesh ../../meshes/front_fib/direct_a.msh \
+    -dim 2            \
+    -mesh_gmsh        \
+    -normal           \
+    -tf 1.0           \
+    -dt 1.0           \
+    -pc_type jacobi   \
+    -nnz_factor 3     \
+    -ksp_type cg \
+    -ksp_atol 1.0e-20 \
+    -ksp_ktol 1.0e-20 \
+    -ksp_rtol 1.0e-13 \
+    -eps_nev  2       \
+    -print_vtu        \
+    -options_left 0
+
+#-pc_type  jacobi lu \
+#-ksp_type cg \
+#-ksp_atol 1.0e-22 \
+#-boundary_2 "X1 001 0 0 1"
+#-ksp_type cg      \
+#-print_petsc      \
