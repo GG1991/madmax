c1c0d0efb65b83741ba0acb25cb9fb2831303788
GG1991 <giuntoli1991@gmail.com>
2017-11-22 15:54:37 +0100

    exs/micro_alone/run.sh

--- exs/micro_alone/run.sh
+++ exs/micro_alone/run.sh
@@ -8,31 +8,18 @@ else
   NP=$1;
 fi
 
+#$MPIEXEC -np $NP xterm -e gdb -x file.gdb --args ../../micro/micro \
 $MPIEXEC -np $NP  ../../micro/micro \
-    -dim 2 \
-    -struct_n      50,50 \
-    -struct_l      0.1,0.1 \
+    -material      "MATRIX TYPE_0 1.0e7 1.0e6 0.3","FIBER TYPE_0 1.0e7 1.0e7 0.3" \
     -fiber_cilin   0.03,0.0,0.0,0.0   \
-    -mat_fiber_t0  1.0e6,1.0e7,0.3 \
-    -mat_matrix_t0 1.0e6,1.0e6,0.3 \
-    -pc_type       jacobi \
-    -ksp_type      cg \
-    -print_vtu \
-    -homo_us \
+    -dim 2                 \
+    -struct_n      75,75   \
+    -struct_l      0.1,0.1 \
+    -pc_type       jacobi  \
+    -ksp_type      cg      \
+    -print_vtu             \
+    -homo_us               \
     -options_left 0
-#: -np 1 xterm -e gdb -x file.gdb --args ../../micro/micro \
-#    -dim 2 \
-#    -struct_n      15,15 \
-#    -struct_l      0.1,0.1 \
-#    -fiber_cilin   0.03,0.0,0.0,0.0   \
-#    -mat_fiber_t0  1.0e6,1.0e7,0.3 \
-#    -mat_matrix_t0 1.0e6,1.0e6,0.3 \
-#    -pc_type       jacobi \
-#    -ksp_type      cg \
-#    -print_vtu \
-#    -homo_us \
-#    -options_left 0
-#    -print_petsc \
 
 #xterm -e gdb --args 
 #xterm -e gdb -x file.gdb --args 
