dd80131cb23fd2011d810b440ab9a348ff23c8de
GG1991 <giuntoli1991@gmail.com>
2017-11-23 14:47:51 +0100

    using same distance relation for micro alone than front_fib

--- exs/micro_alone/run.sh
+++ exs/micro_alone/run.sh
@@ -11,10 +11,10 @@ fi
 #$MPIEXEC -np $NP xterm -e gdb -x file.gdb --args ../../micro/micro \
 $MPIEXEC -np $NP  ../../micro/micro \
     -material      "MATRIX TYPE_0 1.0e7 1.0e6 0.3","FIBER TYPE_0 1.0e7 1.0e7 0.3" \
-    -fiber_cilin   0.03,0.0,0.0,0.0   \
+    -fiber_cilin   0.75,0.0,0.0,0.0   \
     -dim 2                 \
     -struct_n      75,75   \
-    -struct_l      0.1,0.1 \
+    -struct_l      3.0,3.0 \
     -pc_type       jacobi  \
     -ksp_type      cg      \
     -print_vtu             \
