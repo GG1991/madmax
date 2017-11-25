954e7f4f0492fcd3a48eae8ea593678c53d662c8
Guido Giuntoli <giuntoli1991@gmail.com>
2017-11-25 12:06:32 +0100

    changes in new implementation reflected on micro_alone

--- exs/micro_alone/run.sh
+++ exs/micro_alone/run.sh
@@ -10,16 +10,15 @@ fi
 
 #$MPIEXEC -np $NP xterm -e gdb -x file.gdb --args ../../micro/micro \
 $MPIEXEC -np $NP  ../../micro/micro \
-    -material      "MATRIX TYPE_0 1.0e7 1.0e6 0.3","FIBER TYPE_0 1.0e7 1.0e7 0.3" \
-    -fiber_cilin   0.75,0.0,0.0,0.0   \
-    -dim 2                 \
     -struct_n      75,75   \
-    -struct_l      3.0,3.0 \
+    -dim           2       \
+    -material      "MATRIX TYPE_0 1.0e7 1.0e6 0.3","FIBER TYPE_0 1.0e7 1.0e7 0.3" \
+    -micro_struct  "fiber_cilin 3.0 3.0 1 1 0.75 0.0 0.0" \
     -pc_type       jacobi  \
     -ksp_type      cg      \
     -print_vtu             \
     -homo_us               \
-    -options_left 0
+    -options_left  0
 
 #xterm -e gdb --args 
 #xterm -e gdb -x file.gdb --args 
