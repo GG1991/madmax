4e64142081c8fadb2329382b2421e07fd6ab0718
GG1991 <giuntoli1991@gmail.com>
2017-11-29 14:10:21 +0100

    formatting myio.h in one line to see better the gcc warning of arguments

--- inc/myio.h
+++ inc/myio.h
@@ -12,13 +12,8 @@
 
 int myio_printf( void *COMM, const char format[], ... );
 
-int myio_get_string_array_command_line(
-    int argc, const char **argv,
-    const char *option_name, int n_str_expect,
-    char ***strings, int *n_str_found);
+int myio_get_string_array_command_line(int argc, const char **argv, const char *option_name, int n_str_expect, char ***strings, int *n_str_found);
 
-int myio_duplicate_argv_char_to_const_char(
-    int argc, char **argv,
-    const char ***argv_dup);
+int myio_duplicate_argv_char_to_const_char(int argc, char **argv, const char ***argv_dup);
 
 #endif
