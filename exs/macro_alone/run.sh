5329f049eeff502243c0a1d820f1df1bb427351b
GG1991 <giuntoli1991@gmail.com>
2017-12-12 11:47:58 -0300

    cleaning run.sh in macro_alone

--- exs/macro_alone/run.sh
+++ exs/macro_alone/run.sh
@@ -14,17 +14,16 @@ $MPIEXEC -np $NP ../../macro/macro \
     -material "MATRIX MAT_ELASTIC 1.0e7 1.0e6 0.3","MICRO MAT_MICRO" \
     -function "0 2 0.0 0.0 1.0 0.0","1 2 0.0 0.0 1.0 0.001" \
     -mesh cube_2d.msh \
-    -dim 2            \
-    -normal           \
-    -tf 1.0           \
-    -dt 0.2           \
-    -ksp_type cg      \
-    -pc_type jacobi   \
-    -part_geom        \
-    -nl_max_its  2    \
-    -eps_nev     2    \
-    -print_vtu        \
-    -print_petsc      \
+    -dim 2 \
+    -normal \
+    -tf 1.0 \
+    -dt 0.2 \
+    -ksp_type cg \
+    -pc_type jacobi \
+    -part_geom \
+    -nl_max_its 2 \
+    -eps_nev 2 \
+    -print_vtu \
     -options_left 0
 
 #xterm -e gdb -x file.gdb --args 
