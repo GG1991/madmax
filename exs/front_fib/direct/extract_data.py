8970ddd22ab1f769b711e067ed90b178dcf1578e
GG1991 <giuntoli1991@gmail.com>
2017-12-13 10:45:58 -0300

    adding movement in y direction front fiber direct simulation

--- exs/front_fib/direct/extract_data.py
+++ exs/front_fib/direct/extract_data.py
@@ -7,10 +7,8 @@ import vtk2numpy as vn
 import os
 import sys
 
-#----------------------------------------------------------------# 
-
 num_files = 6
-displ   = np.zeros( (num_files,1) )
+displ = np.zeros( (num_files,1) )
 force_x = np.zeros( (num_files,1) )
 file_matrix = np.zeros((num_files,2))
 
@@ -29,24 +27,22 @@ for i in range(0,num_files):
   KEYs  = PVTU.GetPointDataInformation().keys()
   KEYs  = PVTU.GetCellDataInformation().keys()
 
-  PlotOverLine1 = PlotOverLine( Input=PVTU, guiName="PlotOverLine1", Source="High Resolution Line Source" )
+  PlotOverLine1 = PlotOverLine(Input=PVTU, guiName="PlotOverLine1", Source="High Resolution Line Source")
   PlotOverLine1.Source.Point1 = [30.0, 00.0, 0.0]
   PlotOverLine1.Source.Point2 = [30.0, 30.0, 0.0]
   PlotOverLine1.Source.Resolution = 1000
   PlotOverLine1.UpdatePipeline()
 
-  stress_pvtu = vn.getPtsData( PlotOverLine1, "stress" )
-  displ_pvtu  = vn.getPtsData( PlotOverLine1, "displ" )
-  leng_pvtu   = vn.getPtsData( PlotOverLine1, "arc_length" )
+  stress_pvtu = vn.getPtsData(PlotOverLine1, "stress")
+  displ_pvtu = vn.getPtsData(PlotOverLine1, "displ")
+  leng_pvtu = vn.getPtsData(PlotOverLine1, "arc_length")
 
-  displ[i]   = displ_pvtu[0,0]
+  displ[i] = displ_pvtu[0,0]
   force_x[i] = np.trapz(stress_pvtu[:,0],leng_pvtu)
   file_matrix[i] = [ displ[i], force_x[i] ]
 
 
 np.savetxt("direct.dat", file_matrix)
-
-#----------------------------------------------------------------# 
 
 #kd  = fx_direct/disp0  
 #print "k_direct   = ", kd  ,"error =",(kd - kd)/kd *100,"%"
--- exs/front_fib/direct/run_direct.sh
+++ exs/front_fib/direct/run_direct.sh
@@ -9,26 +9,54 @@ else
   NP=$1;
 fi
 
-#$MPIEXEC -np $NP xterm -e gdb -x file.gdb --args $EXECUTABLE  \
+if [ -d "force_x" ]; then
+  rm -rf force_x/*
+else
+  mkdir force_x
+fi
+
 $MPIEXEC -np $NP $EXECUTABLE  \
     -boundary "X0 11 0 0","X1 11 1 0" \
     -material "MATRIX MAT_ELASTIC 1.0e7 1.0e6 0.3","FIBER MAT_ELASTIC 1.0e7 1.0e6 0.3"\
     -function "0 2 0.0 0.0 1.0 0.0","1 2 0.0 0.0 1.0 0.01" \
-    -mesh     direct_10_10_replaced.msh \
-    -dim 2            \
-    -normal           \
-    -tf 1.0           \
-    -dt 0.2           \
-    -pc_type jacobi   \
-    -nnz_factor 3     \
-    -ksp_type cg      \
-    -ksp_atol 1.0e-24 \
-    -ksp_dtol 1.0e-10 \
-    -ksp_rtol 1.0e-17 \
-    -eps_nev  2       \
-    -print_vtu        \
-    -part_geom        \
+    -mesh direct_10_10_replaced.msh \
+    -dim 2 \
+    -normal \
+    -tf 1.0 \
+    -dt 0.2 \
+    -pc_type jacobi \
+    -nnz_factor 3 \
+    -ksp_type cg \
+    -ksp_atol 1.0e-14 \
+    -ksp_rtol 1.0e-13 \
+    -print_vtu \
     -options_left 0
+
+mv macro_* force_x/.
+
+if [ -d "force_y" ]; then
+  rm -rf force_y/*
+else
+  mkdir force_y
+fi
+
+$MPIEXEC -np $NP $EXECUTABLE  \
+    -boundary "X0 11 0 0","X1 11 0 1" \
+    -material "MATRIX MAT_ELASTIC 1.0e7 1.0e6 0.3","FIBER MAT_ELASTIC 1.0e7 1.0e6 0.3"\
+    -function "0 2 0.0 0.0 1.0 0.0","1 2 0.0 0.0 1.0 -0.01" \
+    -mesh direct_10_10_replaced.msh \
+    -dim 2 \
+    -normal 1 \
+    -tf 1.0 \
+    -dt 0.2 \
+    -pc_type jacobi \
+    -ksp_type cg \
+    -ksp_atol 1.0e-14 \
+    -ksp_rtol 1.0e-13 \
+    -print_vtu \
+    -options_left 0
+
+mv macro_* force_y/.
 
 #-pc_type  jacobi lu \
 #-ksp_type cg        \
