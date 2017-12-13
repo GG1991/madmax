192a1b44f88873c536ec5a4f2e3a255524b32f89
GG1991 <giuntoli1991@gmail.com>
2017-12-13 11:01:29 -0300

    cleaning trace.c and .h

--- inc/trace.h
+++ inc/trace.h
@@ -1,9 +1,9 @@
-#include <mpi.h>
-
 #ifndef _TRACE_H_
 #define _TRACE_H_
 
-typedef struct _trace_t{
+#include <mpi.h>
+
+typedef struct{
 
   FILE   *file;
   double t0;
@@ -13,8 +13,8 @@ typedef struct _trace_t{
 
 trace_t trace;
 
-int init_trace( MPI_Comm COMM, const char *file_name );
-int save_event( MPI_Comm COMM, const char *event );
-int end_trace ( MPI_Comm COMM );
+int init_trace(MPI_Comm COMM, const char *file_name);
+int save_event(MPI_Comm COMM, const char *event);
+int end_trace(MPI_Comm COMM);
 
 #endif
--- src/trace.c
+++ src/trace.c
@@ -2,66 +2,59 @@
 #include <stdlib.h>
 #include "trace.h"
 
-/**************************************************/
 
-int init_trace( MPI_Comm COMM, const char *file_name )
-{
+int init_trace(MPI_Comm COMM, const char *file_name){
 
-  int i, rank, size;
-  MPI_Comm_rank( COMM, &rank );
-  MPI_Comm_size( COMM, &size );
+  int rank, size;
+  MPI_Comm_rank(COMM, &rank);
+  MPI_Comm_size(COMM, &size);
 
   trace.t0 = MPI_Wtime();
   
-  if( rank == 0 ){
-    trace.file = fopen( file_name , "w" );
-    fprintf( trace.file, "%-10s", "rank" );
-    for( i = 0 ; i < size ; i++ )
-      fprintf( trace.file, "%-8d ", i );
-    fprintf( trace.file, "\n" );
+  if(rank == 0){
+    trace.file = fopen(file_name , "w");
+    fprintf(trace.file, "%-10s", "rank");
+    for(int i = 0 ; i < size ; i++)
+      fprintf(trace.file, "%-8d ", i);
+    fprintf(trace.file, "\n");
   }
 
   return 0;
 
 }
 
-/**************************************************/
 
-int save_event( MPI_Comm COMM, const char *event )
-{
+int save_event(MPI_Comm COMM, const char *event){
 
-  int i, ierr, rank, size;
+  int ierr, rank, size;
   MPI_Comm_rank( COMM, &rank );
   MPI_Comm_size( COMM, &size );
 
   double time = MPI_Wtime();
  
-  if( rank == 0 )
-  {
+  if(rank == 0){
+
     double *times = malloc( size * sizeof(double));
     ierr = MPI_Gather( &time, 1, MPI_DOUBLE, times, 1, MPI_DOUBLE, 0, COMM); if(ierr) return 1;
-    fprintf( trace.file, "%-10s", event );
-    for( i = 0 ; i < size ; i++ )
-      fprintf( trace.file, "%1.4lf   ", times[i] - trace.t0 );
-    fprintf( trace.file, "\n" );
+    fprintf( trace.file, "%-10s", event);
+    for(int i = 0 ; i < size ; i++)
+      fprintf( trace.file, "%1.4lf   ", (times[i] - trace.t0));
+    fprintf( trace.file, "\n");
   }
   else
-    ierr = MPI_Gather( &time, 1, MPI_DOUBLE, NULL, 1, MPI_DOUBLE, 0, COMM); if(ierr) return 1;
+    ierr = MPI_Gather(&time, 1, MPI_DOUBLE, NULL, 1, MPI_DOUBLE, 0, COMM); if(ierr) return 1;
 
   return 0;
 }
 
-/**************************************************/
 
-int end_trace ( MPI_Comm COMM )
-{
+int end_trace(MPI_Comm COMM){
+
   int rank;
-  MPI_Comm_rank( COMM, &rank );
+  MPI_Comm_rank(COMM, &rank);
 
-  if( rank == 0 )
-    fclose( trace.file );
+  if(rank == 0)
+    fclose(trace.file);
 
   return 0;
 }
-
-/**************************************************/
