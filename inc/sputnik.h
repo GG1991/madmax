d086357fc9c718067ac7c13ee5b1b5e18beec98c
GG1991 <giuntoli1991@gmail.com>
2017-12-14 15:00:52 -0300

    cleaning sputnik.h!

--- inc/sputnik.h
+++ inc/sputnik.h
@@ -1,69 +1,40 @@
 #ifndef SPUTNIK_H
 #define SPUTNIK_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <gsl/gsl_linalg.h>
 #include "petscksp.h"
 #include "slepceps.h"
 #include "list.h"
 #include "stdbool.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "fem.h"
 #include "gmsh.h"
 #include "material.h"
-#include <gsl/gsl_linalg.h>
 
 #define FORMAT_NULL        0
 #define FORMAT_GMSH        1
 #define FORMAT_ALYA        2
 
-#define PRINT_PETSC        0
-#define PRINT_VTK          1
-#define PRINT_VTU          2
-#define PRINT_VTKPART      4
-#define PRINT_ALL          8
-
 int         partition_algorithm;
 
 char        *myname;
 
-int         rank_wor;               //  rank on world comm
-int         nproc_wor;              //  # of processes (WORLD_COMM)
-int         dim;                    //  problem dimensions 
+int         rank_wor;
+int         nproc_wor;
+int         dim;
 
-int         flag_print;
-bool        flag_reactions;
-bool        flag_first_alloc;
-
-/* Cilindrical Fiber in quad Matrix */
-double      LX, LY, LZ;
-int         flag_fiber_cilin;
-int         nx_fibers;
-int         ny_fibers;
-double      fiber_cilin_r;
-double      fiber_cilin_center_devi[3];
-double      fiber_cilin_vals[4];
-double      center_domain[3];
-
-/* Interpolation over structured grids */
-int         nx_interp;
-int         ny_interp;
-int         nz_interp;
-
-// Structures to save de mesh on CSR format 
-
-int          *elm_id;                 // element property number
+int          *elm_id;
 double       *elmv_centroid;
 
 int read_mesh_elmv( MPI_Comm COMM, char *myname, char *mesh_n, int mesh_f);
 int read_mesh_elmv_CSR_GMSH( MPI_Comm COMM, char *myname, char *mesh_n);
-int read_mesh_elmv_CSR_ALYA( MPI_Comm COMM, char *myname, char *mesh_n);
 
 int clean_vector_qsort(int n, int *input, int **output, int *not_rep);
-int give_repvector_qsort( MPI_Comm * comm, char *myname, int n, int *input, int **output, int *nrep);
+int give_repvector_qsort(MPI_Comm *COMM, char *myname, int n, int *input, int **output, int *nrep);
 
-int give_inter_sort( MPI_Comm COMM,
-    int *array1, int n1, int *array2, int n2, int **reps, int *nreps);
+int give_inter_sort( MPI_Comm COMM, int *array1, int n1, int *array2, int n2, int **reps, int *nreps);
 
 int is_in_vector(int val, int *vector, int size);
 int cmpfunc(const void * a, const void * b);
