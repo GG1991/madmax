f816d1dc12a798fbdaaea2cc02901b8304bf0029
GG1991 <giuntoli1991@gmail.com>
2017-12-13 23:49:07 -0300

    extract data extracts now for direct

--- exs/front_fib/direct/extract_data.py
+++ exs/front_fib/direct/extract_data.py
@@ -8,13 +8,15 @@ import os
 import sys
 
 num_files = 6
-displ = np.zeros( (num_files,1) )
+displ_x = np.zeros( (num_files,1) )
+displ_y = np.zeros( (num_files,1) )
 force_x = np.zeros( (num_files,1) )
-file_matrix = np.zeros((num_files,2))
+force_y = np.zeros( (num_files,1) )
+file_matrix = np.zeros((num_files,4))
 
 for i in range(0,num_files):
 
-  file_in = "macro_t_"+str(i)+".pvtu"
+  file_in = "force_x/macro_t_"+str(i)+".pvtu"
   print "check for file "+file_in,
   try:
      f = open(file_in,'rb')
@@ -37,12 +39,38 @@ for i in range(0,num_files):
   displ_pvtu = vn.getPtsData(PlotOverLine1, "displ")
   leng_pvtu = vn.getPtsData(PlotOverLine1, "arc_length")
 
-  displ[i] = displ_pvtu[0,0]
+  displ_x[i] = displ_pvtu[0,0]
   force_x[i] = np.trapz(stress_pvtu[:,0],leng_pvtu)
-  file_matrix[i] = [ displ[i], force_x[i] ]
 
+for i in range(0,num_files):
+
+  file_in = "force_y/macro_t_"+str(i)+".pvtu"
+  print "check for file "+file_in,
+  try:
+     f = open(file_in,'rb')
+     print " ok"
+  except IOError:
+    print " error, not found"
+    sys.exit()
+
+  PVTU  = XMLPartitionedUnstructuredGridReader(FileName=file_in)
+  KEYs  = PVTU.GetPointDataInformation().keys()
+  KEYs  = PVTU.GetCellDataInformation().keys()
+
+  PlotOverLine1 = PlotOverLine(Input=PVTU, guiName="PlotOverLine1", Source="High Resolution Line Source")
+  PlotOverLine1.Source.Point1 = [30.0, 00.0, 0.0]
+  PlotOverLine1.Source.Point2 = [30.0, 30.0, 0.0]
+  PlotOverLine1.Source.Resolution = 1000
+  PlotOverLine1.UpdatePipeline()
+
+  stress_pvtu = vn.getPtsData(PlotOverLine1, "stress")
+  displ_pvtu = vn.getPtsData(PlotOverLine1, "displ")
+  leng_pvtu = vn.getPtsData(PlotOverLine1, "arc_length")
+
+  displ_y[i] = displ_pvtu[0,1]
+  force_y[i] = np.trapz(stress_pvtu[:,2],leng_pvtu)
+
+for i in range(0,num_files):
+  file_matrix[i] = [ displ_x[i], displ_y[i], force_x[i], abs(force_y[i]) ]
 
 np.savetxt("direct.dat", file_matrix)
-
-#kd  = fx_direct/disp0  
-#print "k_direct   = ", kd  ,"error =",(kd - kd)/kd *100,"%"
