7f01172ad1576c5902d8d32fa9dd041699d1dff1
GG1991 <giuntoli1991@gmail.com>
2017-12-16 15:31:42 -0300

    adding new structures in mesh.h to be more clean

--- inc/mesh.h
+++ inc/mesh.h
@@ -12,55 +12,79 @@
 #define MAX_ADJ_NODES 30
 #define MAX_NUM_OF_BOUNDARIES 4
 
-int      *elmdist;     // number of elements in each procesor
-int       nelm;        // # of local elements
-int      *eptr;        // list of indeces of nodes inside eind
-int      *eind;        // list of nodes for elem "i" [eind[eptr[i]] , eind[eptr[i+1]]) (not including)
+int *elmdist;
+int nelm;
+int *eptr;
+int *eind;
 
-int      *allnods;     // all nodes including locals and ghosts
-int       nallnods;    // <nmynods> + <nghost>
-int      *mynods;      // Original (gmsh) numbers of my nodes
-int       nmynods;     // number of local nodes
-int      *ghost;       // ghosts nodes (global numbering)
-int       nghost;      // number of ghost nodes
-int       ntotnod;     // number of total nodes in the mesh
+int *allnods;
+int nallnods;
+int *mynods;
+int nmynods;
+int *ghost;
+int nghost;
+int ntotnod;
 
-double   *coord;       // nodes' coordinates
+double *coord;
 
-int      *loc2petsc;   // array of size <nmynods> + <nghost>
-                       // returns the position in PETSc matrix & vectors
+int *loc2petsc;
 
 typedef struct{
 
-  char     *name;
-  int       kind;                // boundary kind ( xxx -> to decimal )
-  int      *fnum;                // funtion numbers to evaluate
-  int       ndirpn;              // dirichlet values per node
-  int       nneupn;              // neumann values per node
-  int       ndirix;              // number of dir indeces
-  int       ndir;                // number of dir nodes
-  int      *dir_loc_ixs;         // dirichlet indeces (local)
-  int      *dir_glo_ixs;         // dirichlet indeces (global)
-  double   *dir_val;             // dirichlet values
+  char *name;
+  int kind;
+  int *fnum;
+  int ndirpn;
+  int nneupn;
+  int ndirix;
+  int ndir;
+  int *dir_loc_ixs;
+  int *dir_glo_ixs;
+  double *dir_val;
 
 }mesh_boundary_t;
+
 
 typedef struct{
 
   int dim;
-  int n_elem_local;
-  int n_elem_total;
-  int n_node_local;
-  int n_node_total;
+  int nnods_total;
+  int nnods_local;
+  int nelem_total;
+  int nelm_local;
+  int *eptr;
+  int *eind;
+  int **elements;
   int *local_nodes;
   int *ghost_nodes;
   int *local_ghost_nodes;
+  int *local_to_global;
   double *coord;
   list_t boundary_list;
 
 }mesh_t;
 
 extern mesh_t mesh;
+
+
+typedef struct{
+
+  int dim;
+  int nx, ny, nz;
+  int nex, ney, nez;
+  int nelm;
+  int nnods;
+  int *eptr;
+  int *eind;
+  int **elements;
+  double lx, ly, lz;
+  double hx, hy, hz;
+  double *coord;
+
+}mesh_struct_t;
+
+extern mesh_struct_t mesh_struct;
+
 
 int part_mesh(MPI_Comm COMM, char *myname, double *centroid);
 int reenumerate_PETSc(MPI_Comm COMM);
@@ -73,7 +97,8 @@ int swap_vectors_SCR( int *swap, int nproc, int n,  int *npe,
     int *eptr, int *eind, int *elm_id,
     int *npe_swi, int *eind_swi, int *elm_id_swi,
     int *cuts_npe, int *cuts_eind );
-;
+
 int mesh_fill_boundary_list_from_command_line(command_line_t *command_line, list_t *boundary_list);
+
 
 #endif
